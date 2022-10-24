1d28da9803470565cc0b6938738cb82108d1773c
marcelo <marcelo.schell@intraway.com>
2022-10-24 19:41:57 -0300

    add new report

--- app/controllers/informes/InformesDeudasController.java
+++ app/controllers/informes/InformesDeudasController.java
@@ -30,8 +30,82 @@ import views.html.informes.informeDeuda.*;
 
 public class InformesDeudasController extends Controller {
 	
-	
 	public static Result deudasFotoPorFecha() {
+		DynamicForm formBuscador = form().bindFromRequest();
+		
+		BigDecimal totalCorte = new BigDecimal(0);
+		BigDecimal totalHoy = new BigDecimal(0);
+		BigDecimal totalDiferencia = new BigDecimal(0);
+		
+		BigDecimal totalCorteE = new BigDecimal(0);
+		BigDecimal totalHoyE = new BigDecimal(0);
+		BigDecimal totalDiferenciaE = new BigDecimal(0);
+		
+		BigDecimal totalCorteO = new BigDecimal(0);
+		BigDecimal totalHoyO = new BigDecimal(0);
+		BigDecimal totalDiferenciaO = new BigDecimal(0);
+		String fString= null;
+		String fechaHoyString= null;
+		if(!RequestVar.get("fecha_corte").isEmpty()){
+			fString = RequestVar.get("fecha_corte");
+			
+			Date fechaHoy = new Date();
+			SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+			fechaHoyString = simpleDateFormat.format(fechaHoy);
+			
+			Date fecha = DateUtils.formatDate(RequestVar.get("fecha_corte"), "dd/MM/yyyy"); 
+			
+			List<SqlRow> requipo = HistorialDeudaProveedores.getTotalesPorCorteFechaNuevo(true,fecha,true,false);
+			for(SqlRow e : requipo) {
+				totalCorte = totalCorte.add(e.getBigDecimal("total_deuda"));
+				totalCorteE = totalCorteE.add(e.getBigDecimal("total_deuda"));
+			}
+			
+			List<SqlRow> rtotros = HistorialDeudaProveedores.getTotalesPorCorteFechaNuevo(true,fecha,false,false);
+			for(SqlRow o : rtotros) {
+				totalCorte = totalCorte.add(o.getBigDecimal("total_deuda"));
+				totalCorteO = totalCorteO.add(o.getBigDecimal("total_deuda"));
+			}
+			
+			List<SqlRow> requipoHoy = HistorialDeudaProveedores.getTotalesPorCorteFechaNuevo(true,fecha,true,true);
+			for(SqlRow e : requipoHoy) {
+				totalHoy = totalHoy.add(e.getBigDecimal("total_deuda"));
+				totalHoyE = totalHoyE.add(e.getBigDecimal("total_deuda"));
+			}
+			
+			List<SqlRow> rtotrosHoy = HistorialDeudaProveedores.getTotalesPorCorteFechaNuevo(true,fecha,false,true);
+			for(SqlRow o : rtotrosHoy) {
+				totalHoy = totalHoy.add(o.getBigDecimal("total_deuda"));
+				totalHoyO = totalHoyO.add(o.getBigDecimal("total_deuda"));
+			}
+			
+			 
+			
+			
+			
+			
+			
+			totalDiferencia = totalCorte.subtract(totalHoy);
+			totalDiferenciaE = totalCorteE.subtract(totalHoyE);
+			totalDiferenciaO = totalCorteO.subtract(totalHoyO);
+			
+			
+			
+			
+			
+		}else {
+			flash("error", "Debe Seleccionar una Fecha de Corte.");
+		}
+		
+		
+		return ok(deudaFotoPorFecha.render(formBuscador,fString,fechaHoyString,
+				totalCorte,totalHoy,totalDiferencia,
+				totalCorteE,totalHoyE,totalDiferenciaE,
+				totalCorteO,totalHoyO,totalDiferenciaO));
+	}
+	
+	
+	/*public static Result deudasFotoPorFechaOld() {
 		DynamicForm formBuscador = form().bindFromRequest();
 		
 		BigDecimal totalCorte = new BigDecimal(0);
@@ -91,7 +165,7 @@ public class InformesDeudasController extends Controller {
 		return ok(deudaFotoPorFecha.render(formBuscador,fString,fechaHoyString,totalCorte,totalHoy,totalDiferencia
 				,totalCorteE,totalHoyE,totalDiferenciaE
 				,totalCorteO,totalHoyO,totalDiferenciaO));
-	}	
+	}	*/
 	
 	public static Result deudasGenerales() {
 		
