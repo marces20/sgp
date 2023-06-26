88b43bd07e97789b2380ab3cdb25799e923ad2fd
marcelo <marcelo.schell@intraway.com>
2023-06-26 11:03:11 -0300

    add comentario

--- app/controllers/contabilidad/OrdenesPagosCircuitosAccionesController.java
+++ app/controllers/contabilidad/OrdenesPagosCircuitosAccionesController.java
@@ -27,37 +27,37 @@ import controllers.auth.CheckPermiso;
 
 @Security.Authenticated(Secured.class)
 public class OrdenesPagosCircuitosAccionesController extends Controller {
-	
+
 	@CheckPermiso(key = "ordenesPagosCircuitoEditar")
 	public static Result modalCargarExpedienteFisico() {
 		return ok(modalCargarExpedienteFisico.render(form().bindFromRequest()));
 	}
-	
+
 	public static Result cargarExpedienteFisico() {
 		DynamicForm d = form().bindFromRequest();
 		d.discardErrors();
-		
+
 		List<Integer> opSeleccionados = getSeleccionados();
 
 		if(opSeleccionados.isEmpty()) {
 			flash("error", "Seleccione al menos una OP.");
 			return ok(modalCargarExpedienteFisico.render(d));
-		}	
-		
+		}
+
 		Integer idExpediente = null;
 		if(!request().body().asFormUrlEncoded().get("expediente_fisico_id")[0].isEmpty()){
 			idExpediente = new Integer(request().body().asFormUrlEncoded().get("expediente_fisico_id")[0]);
-		}		
-		
+		}
+
 		if(!soloBorrador(opSeleccionados)) {
 			flash("error", "Solo se puede modificar registros en estado borrador.");
 			return ok(modalCargarExpedienteFisico.render(d));
 		}
-		
-		 
+
+
 		if(d.hasErrors())
-			return ok(modalCargarExpedienteFisico.render(d));	
-		
+			return ok(modalCargarExpedienteFisico.render(d));
+
 		ObjectNode result = Json.newObject();
 		try {
 			Integer count = OrdenPagoCircuito.CargarExpedienteFisico(idExpediente, opSeleccionados);
@@ -70,41 +70,41 @@ public class OrdenesPagosCircuitosAccionesController extends Controller {
 			return ok(modalCargarExpedienteFisico.render(d));
 		}
 	}
-	
+
 	@CheckPermiso(key = "ordenesPagosCircuitoPasarBorrador")
 	public static Result modalPasarBorrador() {
 		return ok(modalPasarBorrador.render(form().bindFromRequest()));
 	}
-	
+
 	public static Result pasarBorradorMasivo() {
 		DynamicForm d = form().bindFromRequest();
 		d.discardErrors();
-		
+
 		List<Integer> opcSeleccionados = getSeleccionados();
 
 		if(opcSeleccionados.isEmpty()) {
 			flash("error", "Seleccione al menos un circuito.");
 			return ok(modalPasarBorrador.render(d));
-		}	
-		
+		}
+
 		if(!soloCancelado(opcSeleccionados)) {
 			flash("error", "Solo se puede modificar registros en estado en cancelado.");
 			return ok(modalPasarBorrador.render(d));
 		}
-		
+
 		if(d.hasErrors())
-			return ok(modalPasarBorrador.render(d));	
-		
-		
-		
+			return ok(modalPasarBorrador.render(d));
+
+
+
 		ObjectNode result = Json.newObject();
 		try {
-			
+
 			List<OrdenPagoCircuito> ocx = OrdenPagoCircuito.find.where().in("id", opcSeleccionados).findList();
 			List<Integer> idsExpedientes = OrdenPagoCircuito.getIdsExpedientes(ocx);
-			
+
 			Integer count2 = ExpedienteMovimiento.pasarOtroServicioMasivoConOps(idsExpedientes,ocx, Organigrama.TESORERIA, "");
-			 
+
 			Integer count = OrdenPagoCircuito.modificarEstadoMasivo(Estado.ORDEN_PAGO_CIRCUITO_ESTADO_BORRADOR, opcSeleccionados);
 			result.put("success", true);
 			flash("success", "Se actualizaron " + count + " registros de "+ opcSeleccionados.size() +" seleccionados.");
@@ -114,51 +114,60 @@ public class OrdenesPagosCircuitosAccionesController extends Controller {
 			flash("error", "No se puede modificar los registros.");
 			return ok(modalPasarBorrador.render(d));
 		}
-		
+
 	}
-	
+
 	@CheckPermiso(key = "ordenesPagosCircuitoPasarContabilidad")
-	public static Result modalPasarContabilidad() {
+	public static Result modalPasarContabilidad(Long id) {
 		return ok(modalPasarContabilidad.render(form().bindFromRequest()));
 	}
-	
+
 	public static Result pasarContabilidadMasivo() {
 		DynamicForm d = form().bindFromRequest();
 		d.discardErrors();
-		
+
 		List<Integer> opcSeleccionados = getSeleccionados();
 
 		if(opcSeleccionados.isEmpty()) {
 			flash("error", "Seleccione al menos un circuito.");
 			return ok(modalPasarContabilidad.render(d));
-		}	
-		
+		}
+
 		if(!soloBorrador(opcSeleccionados)) {
 			flash("error", "Solo se puede modificar registros en estado en borrador.");
 			return ok(modalPasarContabilidad.render(d));
 		}
-		
+
+		String comentario= null;
+		try {
+			comentario = request().body().asFormUrlEncoded().get("comentario")[0].toString();
+		} catch (NullPointerException e) {
+		}
+
+
 		if(d.hasErrors())
-			return ok(modalPasarContabilidad.render(d));	
-		
+			return ok(modalPasarContabilidad.render(d));
+
 		ObjectNode result = Json.newObject();
 		try {
+
 			List<OrdenPagoCircuito> ocx = OrdenPagoCircuito.find.where().in("id", opcSeleccionados).findList();
+
 			List<Integer> idsExpedientes = OrdenPagoCircuito.getIdsExpedientes(ocx);
-			
+
 			List<Long> soloDeMiServicio = ExpedienteMovimiento.getStringIsNotMovimientoServicioUsuario(idsExpedientes, Usuario.getUsurioSesion().organigrama_id);
 			if(soloDeMiServicio.size() > 0) {
 				String error = "Solo se puede modificar realizar movimientos donde el expediente se encuentren en mi servicio "+Usuario.getUsurioSesion().organigrama.nombre+" <br>";
 				flash("error", error);
 				return ok(modalPasarContabilidad.render(d));
 			}
-			
-			
-			
-			Integer count2 = ExpedienteMovimiento.pasarOtroServicioMasivoConOps(idsExpedientes,ocx, Organigrama.CONTABILIDAD, "");
-			
+
+
+
+			Integer count2 = ExpedienteMovimiento.pasarOtroServicioMasivoConOps(idsExpedientes,ocx, Organigrama.CONTABILIDAD, comentario);
+
 			Logger.debug("222222222 "+ocx);
-			
+
 			Integer count = OrdenPagoCircuito.modificarEstadoMasivo(Estado.ORDEN_PAGO_CIRCUITO_ESTADO_CONTABILIDAD, opcSeleccionados);
 			result.put("success", true);
 			flash("success", "Se actualizaron " + count + " registros de "+ opcSeleccionados.size() +" seleccionados.");
@@ -168,36 +177,36 @@ public class OrdenesPagosCircuitosAccionesController extends Controller {
 			flash("error", "No se puede modificar los registros.");
 			return ok(modalPasarContabilidad.render(d));
 		}
-		
+
 	}
-	
+
 	@CheckPermiso(key = "ordenesPagosCircuitoPasarRendiciones")
 	public static Result modalPasarRendiciones() {
 		return ok(modalPasarRendiciones.render(form().bindFromRequest()));
 	}
-	
+
 	public static Result pasarRendicionesMasivo() {
 		DynamicForm d = form().bindFromRequest();
 		d.discardErrors();
-		
+
 		List<Integer> opcSeleccionados = getSeleccionados();
 
 		if(opcSeleccionados.isEmpty()) {
 			flash("error", "Seleccione al menos un circuito.");
 			return ok(modalPasarRendiciones.render(d));
-		}	
-		
+		}
+
 		if(!soloContabilidad(opcSeleccionados)) {
 			flash("error", "Solo se puede modificar registros en estado en contabilidad.");
 			return ok(modalPasarRendiciones.render(d));
 		}
-		
+
 		if(d.hasErrors())
-			return ok(modalPasarRendiciones.render(d));	
-		
+			return ok(modalPasarRendiciones.render(d));
+
 		ObjectNode result = Json.newObject();
 		try {
-			
+
 			List<OrdenPagoCircuito> ocx = OrdenPagoCircuito.find.where().in("id", opcSeleccionados).findList();
 			List<Integer> idsExpedientes = OrdenPagoCircuito.getIdsExpedientes(ocx);
 			List<Long> soloDeMiServicio = ExpedienteMovimiento.getStringIsNotMovimientoServicioUsuario(idsExpedientes, Usuario.getUsurioSesion().organigrama_id);
@@ -206,9 +215,9 @@ public class OrdenesPagosCircuitosAccionesController extends Controller {
 				flash("error", error);
 				return ok(modalPasarRendiciones.render(d));
 			}
-			
+
 			Integer count2 = ExpedienteMovimiento.pasarOtroServicioMasivoConOps(idsExpedientes,ocx, Organigrama.RENDICIONES, "");
-			
+
 			Integer count = OrdenPagoCircuito.modificarEstadoMasivo(Estado.ORDEN_PAGO_CIRCUITO_ESTADO_RENDICIONES, opcSeleccionados);
 			result.put("success", true);
 			flash("success", "Se actualizaron " + count + " registros de "+ opcSeleccionados.size() +" seleccionados.");
@@ -218,33 +227,33 @@ public class OrdenesPagosCircuitosAccionesController extends Controller {
 			flash("error", "No se puede modificar los registros.");
 			return ok(modalPasarRendiciones.render(d));
 		}
-		
+
 	}
-	
+
 	@CheckPermiso(key = "ordenesPagosCircuitoPasarRendido")
 	public static Result modalPasarRendido() {
 		return ok(modalPasarRendido.render(form().bindFromRequest()));
 	}
-	
+
 	public static Result pasarRendidoMasivo() {
 		DynamicForm d = form().bindFromRequest();
 		d.discardErrors();
-		
+
 		List<Integer> opcSeleccionados = getSeleccionados();
 
 		if(opcSeleccionados.isEmpty()) {
 			flash("error", "Seleccione al menos un circuito.");
 			return ok(modalPasarRendido.render(d));
-		}	
-		
+		}
+
 		if(!soloRendiciones(opcSeleccionados)) {
 			flash("error", "Solo se puede modificar registros en estado en rendiciones.");
 			return ok(modalPasarRendido.render(d));
 		}
-		
+
 		if(d.hasErrors())
-			return ok(modalPasarRendido.render(d));	
-		
+			return ok(modalPasarRendido.render(d));
+
 		ObjectNode result = Json.newObject();
 		try {
 			Integer count = OrdenPagoCircuito.modificarEstadoMasivo(Estado.ORDEN_PAGO_CIRCUITO_ESTADO_RENDIDO, opcSeleccionados);
@@ -256,28 +265,28 @@ public class OrdenesPagosCircuitosAccionesController extends Controller {
 			flash("error", "No se puede modificar los registros.");
 			return ok(modalPasarRendido.render(d));
 		}
-		
+
 	}
-	
+
 	@CheckPermiso(key = "ordenesPagosCircuitoPasarCancelado")
 	public static Result modalPasarCancelado() {
 		return ok(modalPasarCancelado.render(form().bindFromRequest()));
 	}
-	
+
 	public static Result pasarCanceladoMasivo() {
 		DynamicForm d = form().bindFromRequest();
 		d.discardErrors();
-		
+
 		List<Integer> opcSeleccionados = getSeleccionados();
 
 		if(opcSeleccionados.isEmpty()) {
 			flash("error", "Seleccione al menos un circuito.");
 			return ok(modalPasarCancelado.render(d));
-		}	
-		
+		}
+
 		if(d.hasErrors())
-			return ok(modalPasarCancelado.render(d));	
-		
+			return ok(modalPasarCancelado.render(d));
+
 		ObjectNode result = Json.newObject();
 		try {
 			Integer count = OrdenPagoCircuito.modificarEstadoMasivo(Estado.ORDEN_PAGO_CIRCUITO_ESTADO_CANCELADO, opcSeleccionados);
@@ -289,10 +298,10 @@ public class OrdenesPagosCircuitosAccionesController extends Controller {
 			flash("error", "No se puede modificar los registros.");
 			return ok(modalPasarCancelado.render(d));
 		}
-		
+
 	}
-	
-	
+
+
 	public static Boolean soloBorrador(List<Integer> opcSeleccionados) {
 		return OrdenPagoCircuito.find.where().ne("estado_id", Estado.ORDEN_PAGO_CIRCUITO_ESTADO_BORRADOR).in("id", opcSeleccionados).findRowCount() == 0;
 	}
@@ -305,14 +314,14 @@ public class OrdenesPagosCircuitosAccionesController extends Controller {
 	public static Boolean soloCancelado(List<Integer> opcSeleccionados) {
 		return OrdenPagoCircuito.find.where().ne("estado_id", Estado.ORDEN_PAGO_CIRCUITO_ESTADO_CANCELADO).in("id", opcSeleccionados).findRowCount() == 0;
 	}
-	
+
 	public static List<Integer> getSeleccionados(){
 		String[] checks = null;
 		try {
 			checks = request().body().asFormUrlEncoded().get("id_pago[]");
 		} catch (NullPointerException e) {
 		}
-		
+
 		List<Integer> ids = new ArrayList<Integer>();
 		if(checks != null) {
 			for (String id : checks) {
