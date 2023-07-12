9294bd9bfe1b7ca3f987ea66a7f625b22165e7be
marcelo <marcelo.schell@intraway.com>
2023-07-11 22:44:15 -0300

    add sigla

--- app/models/OrdenSubrubro.java
+++ app/models/OrdenSubrubro.java
@@ -12,28 +12,31 @@ import javax.persistence.Table;
 import play.data.validation.Constraints.Required;
 import play.db.ebean.Model;
 
- 
+
 @Entity
 @Table(name = "ordenes_subrubros")
 public class OrdenSubrubro extends Model {
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="ordenes_subrubros_id_seq")
     public Integer id;
-	
+
 	@Required(message="Debe escribir el nombre")
 	public String nombre;
 
+	@Required(message="Debe escribir el sigla")
+	public String sigla;
+
 	@ManyToOne
 	public OrdenRubro ordenRubro = new OrdenRubro();
-	
+
 	public static Model.Finder<Long,OrdenSubrubro> find = new Model.Finder<Long,OrdenSubrubro>(Long.class, OrdenSubrubro.class);
-	
+
 	public static List<OrdenSubrubro> getOrdenSubrubro(int rubroId){
-		return find.where().eq("orden_rubro_id", rubroId).orderBy("nombre").findList();
+		return find.where().eq("orden_rubro_id", rubroId).orderBy("sigla").findList();
 	}
 
-	
+
 }
