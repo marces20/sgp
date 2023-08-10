56d72e127c5b4d92986c67ed58d9b119c4969c69
marce <marces2000@gmail.com>
2023-08-10 07:46:42 -0300

    fix

--- app/models/TipoResidencia.java
+++ app/models/TipoResidencia.java
@@ -12,54 +12,56 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import play.data.validation.Constraints.Required;
 import play.db.ebean.Model;
 import play.db.ebean.Model.Finder;
 import utils.pagination.Pagination;
 
-@Entity 
+@Entity
 @Table(name = "tipo_residencias")
 public class TipoResidencia extends Model{
-	
+
 private static final long serialVersionUID = 1L;
-	
-	@Id  
+
+	@Id
 	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="tipo_residencias_id_seq")
-	public Long id; 
+	public Long id;
+	@Required(message="Debe escribir un nombre")
 	public String nombre;
-	
+
 	public String tipo;
-	
+
 	@ManyToOne
 	@JoinColumn(name="create_usuario_id", referencedColumnName="id", insertable=false, updatable=false)
-	public Usuario create_usuario; 
+	public Usuario create_usuario;
 	@Column(name="create_usuario_id")
 	public Long create_usuario_id;
-	 
-	public Date create_date; 
-	public Date write_date; 
-	
+
+	public Date create_date;
+	public Date write_date;
+
 	@ManyToOne
 	@JoinColumn(name="write_usuario_id", referencedColumnName="id", insertable=false, updatable=false)
-	public Usuario write_usuario; 
+	public Usuario write_usuario;
 	@Column(name="write_usuario_id")
 	public Long write_usuario_id;
-	
+
 	public static Model.Finder<Long,TipoResidencia> find = new Finder<Long,TipoResidencia>(Long.class, TipoResidencia.class);
-	
+
 	public List<TipoResidencia> getDataSuggest(String input,Integer limit){
 		List<TipoResidencia> l = find.where()
 				.ilike("nombre", "%"+input+"%")
 				.setMaxRows(limit).orderBy("nombre")
-			    .findList();  
-		
+			    .findList();
+
 		return l;
 	}
-	
-	public static Pagination<TipoResidencia> page(String nombre) {    	
+
+	public static Pagination<TipoResidencia> page(String nombre) {
     	Pagination<TipoResidencia> p = new Pagination<TipoResidencia>();
     	p.setOrderDefault("DESC");
     	p.setSortByDefault("id");
-    	
+
     	p.setExpressionList(
     			find.where()
                 .ilike("nombre", "%" + nombre + "%")
