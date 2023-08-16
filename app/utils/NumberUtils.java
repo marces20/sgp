23485510914e75657634188c59edda9183f6016b
marcelo.schell@intraway.com <marcelo.schell@intraway.com>
2023-08-16 18:13:34 -0300

    new report

--- app/utils/NumberUtils.java
+++ app/utils/NumberUtils.java
@@ -9,27 +9,27 @@ import java.util.Currency;
 import java.util.Locale;
 
 public class NumberUtils {
-	
+
 	public static String agregarCerosAlaIzquierda(int numero,int cantidadDeDigitos) {
 		String x = Integer.toString(numero);
-		
+
 		if(x.length() < cantidadDeDigitos) {
 			int len = x.length();
 			String z = "";
 			while(len < cantidadDeDigitos) {
-				z += "0"; 
+				z += "0";
 				len ++;
 			}
 			x = z+x;
-			
+
 		}
-		
-		
+
+
 		return x;
 	}
-	
+
 	public static String formatNumber(Double number,int decimales) {
-		
+
 		DecimalFormatSymbols simbolos = new DecimalFormatSymbols();
 		simbolos.setDecimalSeparator(',');
 		DecimalFormat myFormatter = new DecimalFormat("###,###,##0.0",simbolos);
@@ -50,74 +50,78 @@ public class NumberUtils {
 	           System.out.println("error" );
 	           break;
 	      }
-		
+
 		String output = myFormatter.format(number);
-		
+
 		return output;
 	}
-	
+
 	public static String formatNumber(Double number) {
 		return formatNumber(number,2);
 	}
 
 	public static String moneda(BigDecimal monto, int maximoFraccion, Integer tipo_moneda){
 		monto = (monto == null || monto.compareTo(BigDecimal.ZERO) == 0)?new BigDecimal(0):monto;
-		
+
 		Currency currency = Currency.getInstance("ARS");
 		if (tipo_moneda.equals(1)) {
 			currency = Currency.getInstance("USD");
 		}else if(tipo_moneda.equals(2)) {
 			currency = Currency.getInstance("EUR");
 		}
-		
-		
+
+
 		NumberFormat numberFormat = NumberFormat.getCurrencyInstance(Locale.getDefault());
 		numberFormat.setMaximumFractionDigits(maximoFraccion);
 		numberFormat.setCurrency(currency);
 		numberFormat.setRoundingMode(RoundingMode.HALF_UP);
 		return numberFormat.format(monto);
 	}
-	
+
+	public static String monedaSinDecimales(BigDecimal monto){
+		return transFormarMoneda(monto, 0, 0);
+	}
+
 	public static String moneda(BigDecimal monto){
 		return transFormarMoneda(monto, 2, 0);
 	}
-	
+
 	public static String moneda(BigDecimal monto, Integer tipo_moneda){
 		return transFormarMoneda(monto, 2, tipo_moneda);
 	}
-	
+
 	public static String moneda(BigDecimal monto, int maximoFraccion){
 		return transFormarMoneda(monto, maximoFraccion, 0);
 	}
-	
+
 	public static String transFormarMoneda(BigDecimal monto, int maximoFraccion, Integer tipo_moneda){
 		monto = (monto == null || monto.compareTo(BigDecimal.ZERO) == 0)?new BigDecimal(0):monto;
-		
+
 		Currency currency = Currency.getInstance("ARS");
 		if (tipo_moneda.equals(1)) {
 			currency = Currency.getInstance("USD");
 		}else if(tipo_moneda.equals(2)) {
 			currency = Currency.getInstance("EUR");
 		}
-			
+
 		NumberFormat numberFormat = NumberFormat.getCurrencyInstance(Locale.getDefault());
 		numberFormat.setMaximumFractionDigits(maximoFraccion);
 		numberFormat.setCurrency(currency);
 		numberFormat.setRoundingMode(RoundingMode.HALF_UP);
 		return numberFormat.format(monto);
 	}
-	
+
 	public static boolean isInteger(String s) {
-	    try { 
-	        Integer.parseInt(s); 
-	    } catch(NumberFormatException e) { 
-	        return false; 
+	    try {
+	        Integer.parseInt(s);
+	    } catch(NumberFormatException e) {
+	        return false;
 	    } catch(NullPointerException e) {
 	        return false;
 	    }
 	    // only got here if we didn't return false
 	    return true;
 	}
-	
+
 
 }
