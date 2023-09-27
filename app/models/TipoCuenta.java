84044b085ba1d04b9b259ac9a886147b8ee8b9ca
marcelo.schell@intraway.com <marcelo.schell@intraway.com>
2023-09-27 10:26:23 -0300

    ADD

--- app/models/TipoCuenta.java
+++ app/models/TipoCuenta.java
@@ -8,13 +8,13 @@ import javax.persistence.Table;
 
 import play.data.validation.Constraints.Required;
 import play.db.ebean.Model;
- 
 
-@Entity 
+
+@Entity
 @Table(name = "tipo_cuenta")
 public class TipoCuenta extends Model{
 	private static final long serialVersionUID = 1L;
-	
+
 	public static final Long OPERATIVA = new Long(1);
 	public static final Long PROFE = new Long(2);
 	public static final Long PLAN_SUMAR_HEARM = new Long(3);
@@ -23,16 +23,17 @@ public class TipoCuenta extends Model{
 	public static final Long PLAN_SUMAR_MATERNO = new Long(6);
 	public static final Long FONDO_PERMANENTE_MATERNO = new Long(7);
 	public static final Long FONDO_PERMANENTE_OBERA = new Long(8);
-	
-	@Id  														 
+	public static final Long PLAN_SUMAR_OBERA = new Long(9);
+
+	@Id
 	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="tipo_cuenta_id_seq")
 	public Long id;
 	@Required(message="Debe escribir un nombre")
 	public String nombre;
-	
+
 	public String numero;
-	
+
 	public String denominacion;
-	
+
 	public static Model.Finder<Long,TipoCuenta> find = new Model.Finder<Long,TipoCuenta>(Long.class, TipoCuenta.class);
 }
