1f6f019570f7aabb0cac6566e2a55e24cd82c2eb
marcelo.schell@intraway.com <marcelo.schell@intraway.com>
2023-12-20 16:37:14 -0300

    add factura

--- public/javascripts/recupero/recuperoFactura.js
+++ public/javascripts/recupero/recuperoFactura.js
@@ -1,42 +1,42 @@
 $( function () {
 	$('#searchCliente,#searchPlanilla,#searchDeposito').modalSearch();
 	$("#numero_factura").mask("99999999");
-	$('#searchPacienteCarga').click( function() {  
+	$('#searchPacienteCarga').click( function() {
 		var url = $(this).attr("data-url");
 		dialogoSearchPacienteCarga = crearDialogoGeneral(url);
 		dialogoSearchPacienteCarga.dialog({title: "Cargar Paciente",height:400});
 	});
-	
+
 	$('#filtrosEstados button').click( function() {
 		var checkbox = $(this).find(':checkbox');
 		checkbox.is(':checked') ? checkbox.removeAttr('checked') : checkbox.attr('checked', 'checked');
 		$(this).closest('form').submit();
 	});
 	$('#filtrosEstados button:has(:checkbox:checked)').addClass('activeFiltro');
-	
-	
+
+
 	var options = {
 			script:"/suggestCliente/",
 			varname:"",
 			json:true,
 			shownoresults:true,
 			maxresults:6,
-			callback: function (obj) { 
-										$("#cliente").next().val(obj.id); 
+			callback: function (obj) {
+										$("#cliente").next().val(obj.id);
 									 }
 		};
 	var as_json = new bsn.AutoSuggest('cliente', options);
-	
-	$('#reportePlanilla').click( function() {  
+
+	$('#reportePlanilla').click( function() {
 		var url = $(this).attr("data-url");
 		dialogoPasarBorrador = crearDialogoGeneral(url);
 		dialogoPasarBorrador.dialog({title: "Reporte Planilla Diaria"});
 	});
-	
+
 	$('#reporteRecuperoFacturaExcel').click( function() { //abrir modal para mostrar mensaje informe rentas
 		var url = $(this).attr("data-url");
 		var dialogo = $('<div></div>');
-			 
+
 		dialogo.dialog({
 			title: "Reporte",
 	    	resizable: false,
@@ -55,7 +55,35 @@ $( function () {
 		    open: function( event, ui ) {
 				$.post(url, getCheckSeleccionados(), function(data){
 					dialogo.html(data);
-				});	
+				});
+		    }
+	    });
+	});
+
+	$('#imprimirFacturaAfip').click( function() { //abrir modal para mostrar mensaje informe rentas
+
+		var url = $(this).attr("data-url");
+		var dialogo = $('<div></div>');
+
+		dialogo.dialog({
+			title: "Factura Afip",
+	    	resizable: false,
+			autoOpen: true,
+			modal: true,
+			height: 250,
+			width:750,
+	        buttons: {
+		          Cerrar: function() {
+		            $( this ).dialog( "destroy" );
+		          }
+		    },
+	    	close: function(event, ui ){
+	    		$(this).dialog( "destroy" );
+	    	},
+		    open: function( event, ui ) {
+				$.get(url, getCheckSeleccionados(), function(data){
+					dialogo.html(data);
+				});
 		    }
 	    });
 	});
@@ -76,13 +104,13 @@ $(document).on("submit", '#formPlanilla', function(){
 	submit.replaceWith(getLoading());
 	$.post(url, data, function(data){
 		if(data.success) {
-			 
+
 			form.replaceWith(data.html);
 		} else {
 			form.replaceWith(data);
 		}
 	});
-	
+
 	return false;
 });
 
@@ -94,26 +122,26 @@ $(document).on("submit", '#formCargaClienteDesdeModal', function(){
 	var data = form.serialize()+'&'+$("input[name='check_listado[]']").serialize();
 	var submit = form.find("button[type='submit']");
 	var numeroOp = form.find("#orden_pago").val();
-	
+
 	var nombre = form.find("#nombre").val();
 	var cuit = form.find("#cuit").val();
 	var dni = form.find("#dni").val();
 	var id_paciente_rismi = form.find("#id_paciente_rismi").val();
 	var referencia = form.find("#referencia").val();
-	
+
 	submit.replaceWith(getLoading());
 	$.post(url, data, function(data){
 		if(data.success) {
-			 
+
 			$("#cliente_id").val(data.idCliente);
 			$("#cliente_nombre").val(data.nombre);
-			
+
 			if(data.profeCliente){
 				$("#profeCliente").html("PROFE")
 			}else{
 				$("#profeCliente").html("")
 			}
-			
+
 			dialogoSearchPacienteCarga.dialog( "destroy" );
 			form.replaceWith(data.html);
 		} else {
@@ -122,9 +150,9 @@ $(document).on("submit", '#formCargaClienteDesdeModal', function(){
 			}else{
 				form.replaceWith(data);
 			}
-			
+
 		}
 	});
-	
+
 	return false;
 });
