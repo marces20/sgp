b775264d51738730347cd3b6ae52d3e1ddf636d4
marcelo.schell@intraway.com <marcelo.schell@intraway.com>
2024-05-10 12:56:37 -0300

    add

--- app/models/recupero/RecuperoPago.java
+++ app/models/recupero/RecuperoPago.java
@@ -42,111 +42,121 @@ import utils.DateUtils;
 import utils.formatters.DecimalComa;
 import utils.pagination.Pagination;
 
-@Entity 
+@Entity
 @Table(name = "recupero_pagos")
 public class RecuperoPago extends Model {
-	
+
 private static final long serialVersionUID = 1L;
-	
+
 	public static final String PAGO_CHEQUE   = "cheque";
 	public static final String PAGO_TARJETA  = "tarjeta";
 	public static final String PAGO_EFECTIVO = "efectivo";
 
-	@Id  														 
+	@Id
 	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="recupero_pagos_id_seq")
 	public Long id;
-	
+
 	@ManyToOne
 	@JoinColumn(name="cliente_id", referencedColumnName="id", insertable=false, updatable=false)
-	public Cliente cliente; 
+	public Cliente cliente;
 	@Required(message="Debe tener un cliente asociado")
 	public Long cliente_id;
-	
+
 	public Long pago_principal_id;
-	
+
 	@Required(message="Debe escribir una fecha")
 	@Formats .DateTime(pattern="dd/MM/yyyy")
 	public Date fecha;
-	
+
 	public String nombre;
 	public String nota;
 	public String concepto;
-	
+
 	public String numero;
-	
+
 	public Boolean parcializada = false;
-	
+
 	@Required(message="Seleccione un tipo de pago")
 	public String tipoPago;
-	
+
 	/*@OneToOne(mappedBy="pago")
 	public Cheque cheque;*/
-	
+
 	@OneToMany()
 	public List<Cheque> cheque;
-	
-	
+
+
 	@ManyToOne
 	@JoinColumn(name="estado_id", referencedColumnName="id", insertable=false, updatable=false)
 	public Estado estado;
-	public Long estado_id; 
-	
+	public Long estado_id;
+
 	@ManyToOne
 	@JoinColumn(name="create_usuario_id", referencedColumnName="id", insertable=false, updatable=false)
-	public Usuario create_usuario; 
+	public Usuario create_usuario;
 	@Column(name="create_usuario_id")
 	public Long create_usuario_id;
-	
-	
+
+
 	@Formats.DateTime(pattern="yyyy-MM-dd HH:mm:ss")
 	@Column(updatable=false)
 	public Date create_date;
 	@Formats.DateTime(pattern="yyyy-MM-dd HH:mm:ss")
 	@Column(updatable=false)
-	public Date write_date; 
-	
+	public Date write_date;
+
 	@ManyToOne
 	@JoinColumn(name="write_usuario_id", referencedColumnName="id", insertable=false, updatable=false)
-	public Usuario write_usuario; 
+	public Usuario write_usuario;
 	@Column(name="write_usuario_id")
 	public Long write_usuario_id;
-	
+
 	@ManyToOne
 	@JoinColumn(name="periodo_id", referencedColumnName="id", insertable=false, updatable=false)
 	public Periodo periodo;
 	public Integer periodo_id;
-	
+
 	@ManyToOne
 	@JoinColumn(name="expediente_id", referencedColumnName="id", insertable=false, updatable=false)
 	public Expediente expediente;
 	public Integer expediente_id;
-	
+
 	@ManyToOne
 	@JoinColumn(name="planilla_id", referencedColumnName="id", insertable=false, updatable=false)
 	public RecuperoPlanilla planilla;
 	@Required(message="Seleccion Planilla")
 	public Integer planilla_id;
-	
+
 	@ManyToOne
 	@JoinColumn(name="recupero_factura_id", referencedColumnName="id", insertable=false, updatable=false)
 	public RecuperoFactura recuperoFactura;
 	public Integer recupero_factura_id;
-	
+
+	@ManyToOne
+	@JoinColumn(name="recupero_nota_credito_id", referencedColumnName="id", insertable=false, updatable=false)
+	public RecuperoNotaCredito recuperoNotaCredito;
+	public Integer recupero_nota_credito_id;
+
+	@ManyToOne
+	@JoinColumn(name="recupero_nota_debito_id", referencedColumnName="id", insertable=false, updatable=false)
+	public RecuperoNotaDebito recuperoNotaDebito;
+	public Integer recupero_nota_debito_id;
+
 	@Transient
 	public String textoTipoPago;
-	
+
 	@DecimalComa(value="")
 	@Required(message="Requiere precio")
 	public BigDecimal total;//Base
 
-	
-	
+
+
     public void setId(Long id) {
         this.id = id;
     }
-	
+
 	public static Model.Finder<Long,RecuperoPago> find = new Finder<Long,RecuperoPago>(Long.class, RecuperoPago.class);
-	
+
 	public String getTextoTipoPago() {
 
 		try {
@@ -163,32 +173,32 @@ private static final long serialVersionUID = 1L;
 				return "";
 			}
 		} catch (Exception es) {
-			
+
 		}
-		
+
 		return "";
-		
+
 	}
-	
+
 	public Long parcializar(Long id){
-		
+
 		Connection conn = null;
 		PreparedStatement stmt = null;
 		ResultSet rs = null;
 		Long ret = new Long(0);
 		try {
-			
+
 			conn = play.db.DB.getConnection();
-			
+
 			stmt = conn.prepareStatement("SELECT parcializar_recupero_pago(?)");
 			stmt.setInt(1, id.intValue());
-			
+
 			rs = stmt.executeQuery();
-			
+
 			if (rs.next()) {
 				ret = (long) rs.getInt(1);
 			}
-			
+
 		}catch (SQLException e) {
 			Logger.error("Error duplicar: "+e);
         } finally {
@@ -196,98 +206,100 @@ private static final long serialVersionUID = 1L;
         	if (rs != null) try { rs.close(); } catch (Exception e) { }
             if (conn != null) try { conn.close(); } catch (Exception e) { }
         }
-		 
+
 		return ret;
 	}
-	
-	
-	public static Pagination<RecuperoPago> page(String nombre, 
-											    String cliente, 
-											    String desde, 
-											    String hasta, 
-											    String tipoPago, 
-											    String filtroBorrador, 
-											    String filtroEnCurso, 
-											    String filtroAprobada, 
+
+
+	public static Pagination<RecuperoPago> page(String nombre,
+											    String cliente,
+											    String desde,
+											    String hasta,
+											    String tipoPago,
+											    String filtroBorrador,
+											    String filtroEnCurso,
+											    String filtroAprobada,
 											    String filtroCancelada,
 											    String numero_factura,
 											    String cliente_tipo_id,
 											    String puntoventa_id,
 											    String deposito,
-											    String planilla_id) {    	
+											    String planilla_id) {
     	Pagination<RecuperoPago> p = new Pagination<RecuperoPago>();
     	p.setOrderDefault("DESC");
     	p.setSortByDefault("id");
-    	
+
        	ExpressionList<RecuperoPago> e = find
        									.fetch("planilla")
        									.fetch("estado")
        									.fetch("recuperoFactura")
        									.fetch("cliente")
+       									.fetch("recuperoNotaCredito")
+       									.fetch("recuperoNotaDebito")
        				 					.where();
-		
+
        	if(!numero_factura.isEmpty()) {
     		e.ilike("recuperoFactura.numero", "%"+numero_factura+"%");
     	}
-       	
+
     	if(!nombre.isEmpty()) {
     		e.ilike("nombre", "%"+nombre+"%");
     	}
     	if(!cliente.isEmpty()) {
     		e.eq("cliente_id", Integer.parseInt(cliente));
     	}
-    	
+
     	if(!deposito.isEmpty()) {
     		e.eq("planilla.deposito_id", Integer.parseInt(deposito));
     	}
-    	
+
     	if(!cliente_tipo_id.isEmpty()){
     		e.eq("cliente.cliente_tipo_id", Integer.parseInt(cliente_tipo_id));
     	}
     	if(!puntoventa_id.isEmpty()){
     		e.eq("recuperoFactura.puntoventa_id", Integer.parseInt(puntoventa_id));
     	}
-    	
+
 		if(!desde.isEmpty()){
     		Date fd = DateUtils.formatDate(desde, "dd/MM/yyyy");
     		e.ge("fecha", fd);
     	}
-		
+
 		if(!hasta.isEmpty()){
     		Date fh = DateUtils.formatDate(hasta, "dd/MM/yyyy");
     		e.le("fecha", fh);
     	}
-    	
+
     	if(!tipoPago.isEmpty()) {
     		e.eq("tipo_pago", tipoPago);
     	}
-		
+
     	if(!filtroBorrador.isEmpty() || !filtroAprobada.isEmpty() || !filtroCancelada.isEmpty() || !filtroEnCurso.isEmpty()) {
     		e = e.disjunction();
 
 	   		if(!filtroBorrador.isEmpty()){
 	   			 e = e.eq("estado_id", Estado.RECUPERO_PAGO_BORRADOR);
 	   		}
-	   		
+
 	   		if(!filtroAprobada.isEmpty()){
 	   			 e = e.eq("estado_id", Estado.RECUPERO_PAGO_PAGADO);
 	   		}
-	       	
+
 	   		if(!filtroCancelada.isEmpty()){
 	   			 e = e.eq("estado_id", Estado.RECUPERO_PAGO_CANCELADO);
 	   		}
-	   		
+
 	   		if(!filtroEnCurso.isEmpty()){
 	   			 e = e.eq("estado_id", Estado.RECUPERO_PAGO_ENCURSO);
 	   		}
 	   		e = e.endJunction();
     	}
-    	
-    	
+
+
     	if(!planilla_id.isEmpty()) {
     		e.eq("planilla_id", Integer.parseInt(planilla_id));
     	}
-    	
+
     	if(!Permiso.check("verTodoRecupero")){
     		if(Usuario.getUsurioSesion().organigrama != null && Usuario.getUsurioSesion().organigrama.deposito != null){
     			e = e.disjunction();
@@ -296,57 +308,57 @@ private static final long serialVersionUID = 1L;
     			e = e.eq("planilla.deposito_id", Usuario.getUsurioSesion().organigrama.deposito_id.intValue());
     			e = e.isNotNull("planilla.deposito_id");
     			e = e.endJunction();
-    			
+
     			e = e.conjunction();
     			e = e.eq("recuperoFactura.planilla.deposito_id", Usuario.getUsurioSesion().organigrama.deposito_id.intValue());
     			e = e.isNull("planilla.deposito_id");
     			e = e.endJunction();
-    			
+
     			e = e.endJunction();
     		}else{
     			e.isNull("planilla.deposito_id");
     		}
     	}
-    	
+
     	p.setExpressionList(e);
     	return p;
 	}
-	
+
 	public static BigDecimal getTotalPagadoPorFecha(Date fdesde,Date fhasta){
-		
-		BigDecimal r = new BigDecimal(0); 
+
+		BigDecimal r = new BigDecimal(0);
 		String where = "";
 		if(fhasta != null && fdesde != null){
 			where += " AND fecha >= :fdesde AND fecha <= :fhasta ";
 		}
-		
+
 		String sql = "SELECT SUM(total) as total " +
 				"FROM recupero_pagos pr " +
 				"WHERE estado_id = :estado "+where;
-				
+
 		SqlQuery s = Ebean.createSqlQuery(sql);
-		
+
 		if(fhasta != null && fdesde != null){
 			s = s.setParameter("fdesde",fdesde);
 			s = s.setParameter("fhasta",fhasta);
 		}
-		
+
 		s = s.setParameter("estado",Estado.RECUPERO_PAGO_PAGADO);
-		
+
 		SqlRow re = s.findUnique();
-		
+
 		if(s != null){
 			r = re.getBigDecimal("total");
 		}
-		
+
 		return r;
 	}
-	
+
 	public boolean controlPermisoDeposito() {
-		boolean r = true; 
+		boolean r = true;
 		if(!Permiso.check("verTodoRecupero")){
 			if(Usuario.getUsurioSesion().organigrama != null && Usuario.getUsurioSesion().organigrama.deposito != null){
-				
+
 				Long dip = null;
 				if(planilla_id != null) {
 					RecuperoPlanilla rp = RecuperoPlanilla.find.byId(planilla_id.longValue());
@@ -355,7 +367,7 @@ private static final long serialVersionUID = 1L;
 					RecuperoFactura rf = RecuperoFactura.find.byId(recupero_factura_id.longValue());
 					dip = rf.planilla.deposito_id;
 				}
-				
+
 				Logger.debug("yyyyyyyyyyyy "+dip);
 				if(Usuario.getUsurioSesion().organigrama.deposito_id.compareTo(dip) != 0) {
 					r = false;
@@ -364,7 +376,7 @@ private static final long serialVersionUID = 1L;
 				r = false;
 			}
 		}
-		
+
 		return r;
 	}
 }
