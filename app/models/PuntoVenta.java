5808eaaeac48cb10562465693992af03cd3dcfa1
Marcelo Schell <mschell@proveedor.teco.com.ar>
2024-07-15 23:09:15 -0300

    add habilitado

--- app/models/PuntoVenta.java
+++ app/models/PuntoVenta.java
@@ -12,27 +12,29 @@ import play.data.validation.Constraints.Required;
 import play.db.ebean.Model;
 import play.db.ebean.Model.Finder;
 
-@Entity 
+@Entity
 @Table(name = "punto_ventas")
 public class PuntoVenta extends Model {
-	
+
 	private static final long serialVersionUID = 1L;
-	
-	@Id  														 
+
+	@Id
 	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="punto_ventas_id_seq")
 	public Long id;
-	
+
 	@ManyToOne
 	@JoinColumn(name="deposito_id", referencedColumnName="id", insertable=false, updatable=false)
 	public Deposito deposito;
 	@Required(message="Requiere una Institucion")
 	public Long deposito_id;
-	
+
 	@Required(message="Requiere una numero")
 	public String numero;
-	
+
 	@Required(message="Requiere tipo facturacion")
 	public String tipo_facturacion;
-	
+
+	public boolean habilitado;
+
 	public static Finder<Long,PuntoVenta> find = new Finder<Long,PuntoVenta>(Long.class, PuntoVenta.class);
 }
