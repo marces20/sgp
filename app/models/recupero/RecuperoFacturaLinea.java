bf9c0ce13a323d617b6f74208b129e28af0805fd
Marcelo Schell <mschell@proveedor.teco.com.ar>
2024-08-01 17:10:29 -0300

    nota

--- app/models/recupero/RecuperoFacturaLinea.java
+++ app/models/recupero/RecuperoFacturaLinea.java
@@ -24,82 +24,84 @@ import play.db.ebean.Model.Finder;
 import utils.formatters.DecimalComa;
 import utils.pagination.Pagination;
 
-@Entity 
+@Entity
 @Table(name = "recupero_factura_lineas")
 public class RecuperoFacturaLinea extends Model {
-	
+
 	private static final long serialVersionUID = 1L;
-	
-	@Id  														 
+
+	@Id
 	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="recupero_factura_lineas_id_seq")
 	public Long id;
-	
+
 	@ManyToOne
 	@JoinColumn(name="recupero_factura_id", referencedColumnName="id", insertable=false, updatable=false)
 	public RecuperoFactura recupero_factura;
 	@Required(message="Debe tener una factura asociado")
 	public Long recupero_factura_id;
-	
+
 	@ManyToOne
 	@JoinColumn(name="producto_id", referencedColumnName="id", insertable=false, updatable=false)
 	public  Producto producto;
 	@Required(message="Requiere producto")
 	public Long producto_id;
-	
+
 	@ManyToOne
 	@JoinColumn(name="cuenta_analitica_id", referencedColumnName="id", insertable=false, updatable=false)
 	public CuentaAnalitica cuentaAnalitica;
 	@Required(message="Requiere Cuenta Presupuestaria")
 	public Long cuenta_analitica_id;
-	
+
 	@ManyToOne
 	@JoinColumn(name="cuenta_id", referencedColumnName="id", insertable=false, updatable=false)
 	public Cuenta cuenta;
 	@Required(message="Requiere Cuenta")
 	public Long cuenta_id;
-	
+
 	@DecimalComa(value="")
 	@Required(message="Requiere precio")
 	public BigDecimal precio;
-	
+
 	@DecimalComa(value="")
 	@Required(message="Requiere cantidad")
 	public BigDecimal cantidad;
-	
+
 	@ManyToOne
 	@JoinColumn(name="udm_id", referencedColumnName="id", insertable=false, updatable=false)
 	public Udm udm;
 	@Required(message="Requiere udm")
 	public Long udm_id;
-	
+
 	@ManyToOne
 	@JoinColumn(name="create_usuario_id", referencedColumnName="id", insertable=false, updatable=false)
-	public Usuario create_usuario; 
+	public Usuario create_usuario;
 	@Column(name="create_usuario_id")
 	public Long create_usuario_id;
-	 
-	public Date create_date; 
-	public Date write_date; 
-	
+
+	public Date create_date;
+	public Date write_date;
+
 	@ManyToOne
 	@JoinColumn(name="write_usuario_id", referencedColumnName="id", insertable=false, updatable=false)
-	public Usuario write_usuario; 
+	public Usuario write_usuario;
 	@Column(name="write_usuario_id")
 	public Long write_usuario_id;
-	
+
+	public String nota;
+
 	public BigDecimal getTotal(){
 		return cantidad.multiply(precio);
 	}
-	
+
 	public static Model.Finder<Long,RecuperoFacturaLinea> find = new Finder<Long,RecuperoFacturaLinea>(Long.class, RecuperoFacturaLinea.class);
-	
-	public static Pagination<RecuperoFacturaLinea> page(Long recuperoFacturaId) {    	
+
+	public static Pagination<RecuperoFacturaLinea> page(Long recuperoFacturaId) {
     	Pagination<RecuperoFacturaLinea> p = new Pagination<RecuperoFacturaLinea>();
     	p.setOrderDefault("ASC");
     	p.setSortByDefault("producto.nombre");
-    	
+
     	p.setExpressionList(find.fetch("producto").where().eq("recupero_factura_id", recuperoFacturaId));
     	return p;
 	}
-	
+
 }
