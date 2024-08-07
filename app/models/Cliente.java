8c00c1bf7d7921f1b3d8f2fb975906f945c1da2a
Marcelo Schell <mschell@proveedor.teco.com.ar>
2024-08-07 12:34:48 -0300

    aff

--- app/models/Cliente.java
+++ app/models/Cliente.java
@@ -207,7 +207,7 @@ public class Cliente extends Model {
 	    	if(dp != null){
 	    		String calle = (dp.calle != null)?dp.calle:"";
 	    		String numero = (dp.numero != null)?dp.numero:"";
-	    		String localidad = (dp.localidad != null)?dp.localidad.nombre:"";
+	    		String localidad = (dp.localidad != null && !dp.localidad.id.equals(new Integer(442)))?dp.localidad.nombre:"";
 	    		String provincia = (dp.localidad != null && dp.localidad.provincia != null)?dp.localidad.provincia.nombre:"";
 	    		String pais = (dp.localidad != null && dp.localidad.provincia != null && dp.localidad.provincia.pais != null)?dp.localidad.provincia.pais.nombre:"";
 	    		direccion = calle+" "+numero+" - "+localidad+" - "+provincia+" - "+pais;
