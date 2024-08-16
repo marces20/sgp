2f2ef3047d03b41d446594260747122a8ae3deb6
Marcelo Schell <mschell@proveedor.teco.com.ar>
2024-08-16 11:25:26 -0300

    add

--- app/controllers/recupero/RecuperoFacturaLineasController.java
+++ app/controllers/recupero/RecuperoFacturaLineasController.java
@@ -27,45 +27,46 @@ import views.html.recupero.recuperoFacturaLineas.*;
 
 @Security.Authenticated(Secured.class)
 public class RecuperoFacturaLineasController extends Controller {
-	
+
 	final static Form<RecuperoFacturaLinea> lineaForm = form(RecuperoFacturaLinea.class);
-	
+
 	public static Result index(Long facturaId, Boolean editable) {
-		
+
 		Pagination<RecuperoFacturaLinea> lineas = RecuperoFacturaLinea.page(facturaId);
 
 		return ok(indexRecuperoFacturaLinea.render(lineas, editable));
 	}
-	
+
 	public static Result crear(String facturaId) {
 		flash().clear();
 		Map<String,String> b = new HashMap<String, String>();
 		b.put("recupero_factura_id", facturaId);
 		b.put("udm_id","1");
+		b.put("cantidad","1");
 		b.put("cuenta_analitica_id",CuentaAnalitica.ARANCELAMIENTO_ID.toString());
 		b.put("cuentaAnalitica.nombre",CuentaAnalitica.ARANCELAMIENTO);
-		b.put("cuenta_id",Cuenta.OTROS_INGRESOS_ID.toString());		
+		b.put("cuenta_id",Cuenta.OTROS_INGRESOS_ID.toString());
 		b.put("cuenta.nombre",Cuenta.OTROS_INGRESOS);
-		
+
 		Form<RecuperoFacturaLinea> linea = form(RecuperoFacturaLinea.class).bind(b);
 		linea.discardErrors();
 		return ok(crearLineaProducto.render(linea));
 	}
-	
+
 	public static Result eliminar(Long id) {
 		ObjectNode restJs = Json.newObject();
-		
+
 		try {
 			RecuperoFacturaLinea.find.byId(id).delete();
 		} catch (PersistenceException pe) {
 			play.Logger.error("excepcion", pe);
 			restJs.put("success", false);
 		}
-		
+
 		restJs.put("success", true);
 		return ok(restJs);
 	}
-	
+
 	public static Result guardar() {
 		Form<RecuperoFacturaLinea> lineaForm = form(RecuperoFacturaLinea.class).bindFromRequest();
 
@@ -85,7 +86,7 @@ public class RecuperoFacturaLineasController extends Controller {
 			flash("error", "No se ha podido almacenar el registro.");
 			return ok(crearLineaProducto.render(lineaForm));
 		}
-		
+
 		RecuperoFacturaLinea linea = RecuperoFacturaLinea.find.where().eq("id", lineaForm.get().id).findUnique();
 		Object c = verLineaProducto.render(linea);
 		ObjectNode restJs = Json.newObject();
@@ -94,17 +95,17 @@ public class RecuperoFacturaLineasController extends Controller {
 		restJs.put("html", c.toString());
 		return ok(restJs);
 	}
-	
+
 	public static Result editar(Long id) {
 		flash().clear();
 		RecuperoFacturaLinea linea = RecuperoFacturaLinea.find.byId(id);
 		return ok(editarLineaProducto.render(lineaForm.fill(linea)));
 	}
-	
+
 	public static Result actualizar() {
 
 		Form<RecuperoFacturaLinea> lineaForm = form(RecuperoFacturaLinea.class).bindFromRequest();
-		
+
 		try {
 			if(lineaForm.hasErrors()) {
 				flash("error", "Error en formulario");
@@ -121,7 +122,7 @@ public class RecuperoFacturaLineasController extends Controller {
 			flash("error", "No se ha podido almacenar el registro.");
 			return ok(editarLineaProducto.render(lineaForm));
 		}
-		
+
 		RecuperoFacturaLinea linea = RecuperoFacturaLinea.find.where().eq("id", lineaForm.get().id).findUnique();
 		Object c = verLineaProducto.render(linea);
 		ObjectNode restJs = Json.newObject();
