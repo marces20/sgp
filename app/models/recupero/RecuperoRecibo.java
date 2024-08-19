3c60bd024740d3103251ab5b336ac9a23ae32560
Marcelo Schell <mschell@proveedor.teco.com.ar>
2024-08-18 21:37:28 -0300

    add

--- app/models/recupero/RecuperoRecibo.java
+++ app/models/recupero/RecuperoRecibo.java
@@ -128,7 +128,7 @@ public class RecuperoRecibo extends Model {
 			p.setOrderDefault("DESC");
 			p.setSortByDefault("id");
 
-			ExpressionList<RecuperoRecibo> e = find.where();
+			ExpressionList<RecuperoRecibo> e = find.fetch("cliente").fetch("planilla").fetch("puntoVenta").where();
 
 			if(!numero.isEmpty()) {
 
