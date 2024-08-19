e2e4f13c9e9c512894171d48060c239540ef10f9
Marcelo Schell <mschell@proveedor.teco.com.ar>
2024-08-19 15:59:17 -0300

    add sucursal_macro_id

--- app/models/Localidad.java
+++ app/models/Localidad.java
@@ -17,28 +17,30 @@ import play.db.ebean.Model;
 public class Localidad extends Model {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="localidades_id_seq")
     public Integer id;
-	
+
 	@Required(message="Debe escribir el nombre", groups=Localidad.class)
 	public String nombre;
-	
+
+	public Integer sucursal_macro_id;
+
 	@ManyToOne
 	public Provincia provincia;
 
 	public static Model.Finder<Long,Localidad> find = new Model.Finder<Long,Localidad>(Long.class, Localidad.class);
-	
+
 	public static List<Localidad> getLocalidades(int provinciaId){
 		return find.where().eq("provincia_id", provinciaId).orderBy("nombre").findList();
 	}
-	
+
 	public List<Localidad> getDataSuggest(String input,Integer limit){
 		List<Localidad> l = find.fetch("provincia").fetch("provincia.pais").where()
 				.ilike("nombre", "%"+input+"%").eq("provincia.pais.id", Pais.ARGENTINA)
 				.setMaxRows(limit).orderBy("nombre")
-			    .findList();  
+			    .findList();
 		return l;
 	}
 
