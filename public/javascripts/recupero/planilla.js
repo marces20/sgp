98174ddbcf2d6049ddf574834ef91b00d7bbb45d
Marcelo Schell <mschell@proveedor.teco.com.ar>
2024-08-28 20:53:03 -0300

    add

--- public/javascripts/recupero/planilla.js
+++ public/javascripts/recupero/planilla.js
@@ -3,7 +3,7 @@ $( function () {
 	$('#searchExpediente,#searchDeposito,#searchDeposito').modalSearch();
 	$("#numero").numeric_input();
 	$('#searchPlanilla').modalSearch();
-	
+
 	$('#reporteDesdePlanilla').click( function() { //abrir modal para mostrar mensaje solicitud 322
 		var url = $(this).attr("data-url");
 		var dialogo = $('<div></div>');
@@ -25,12 +25,38 @@ $( function () {
 		    open: function( event, ui ) {
 				$.post(url,   function(data){
 					dialogo.html(data);
-				});	
+				});
 		    }
 	      });
-		
+
 	});
-	 
-	
+
+	$('#reporteDesdePlanillaPagos').click( function() { //abrir modal para mostrar mensaje solicitud 322
+		var url = $(this).attr("data-url");
+		var dialogo = $('<div></div>');
+		dialogo.dialog({
+			title: "Reporte Planilla",
+	    	resizable: false,
+			autoOpen: true,
+			modal: true,
+			height: 250,
+			width:750,
+	        buttons: {
+		          Cerrar: function() {
+		            $( this ).dialog( "destroy" );
+		          }
+		    },
+	    	close: function(event, ui ){
+	    		$(this).dialog( "destroy" );
+	    	},
+		    open: function( event, ui ) {
+				$.post(url,   function(data){
+					dialogo.html(data);
+				});
+		    }
+	      });
+
+	});
+
+
 });
-	
