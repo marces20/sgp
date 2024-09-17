072bde8fc40a5b981196a017518ccf2d533a9f77
Marcelo Schell <mschell@proveedor.teco.com.ar>
2024-09-17 10:54:41 -0300

    add

---
+++ app/controllers/equipo/EquipoIncidenciaController.java
@@ -0,0 +1,120 @@
+package controllers.equipo;
+
+import static play.data.Form.form;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.persistence.PersistenceException;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import models.equipos.EquipoIncidencia;
+import play.data.Form;
+import play.libs.Json;
+import play.mvc.Controller;
+import play.mvc.Result;
+import utils.pagination.Pagination;
+import views.html.equipo.equipoIncidencia.*;
+
+public class EquipoIncidenciaController extends Controller {
+
+	final static Form<EquipoIncidencia> incidenciaForm = form(EquipoIncidencia.class);
+
+	public static Result index(Long equipoId, Boolean editable) {
+
+		Pagination<EquipoIncidencia> historia = EquipoIncidencia.page(equipoId);
+
+		return ok(indexEquipoIncidencia.render(historia, editable));
+	}
+
+	public static Result crear(String equipoId) {
+		flash().clear();
+		Map<String,String> b = new HashMap<String, String>();
+		b.put("equipo_id", equipoId);
+
+		Form<EquipoIncidencia> linea = form(EquipoIncidencia.class).bind(b);
+		linea.discardErrors();
+		return ok(crearEquipoIncidencia.render(linea));
+	}
+
+	public static Result guardar() {
+		Form<EquipoIncidencia> incidenciaForm = form(EquipoIncidencia.class).bindFromRequest();
+
+		try {
+			if(incidenciaForm.hasErrors()) {
+				System.out.println(incidenciaForm.errors());
+				flash("error", "Error en formulario");
+				return ok(crearEquipoIncidencia.render(incidenciaForm));
+			} else {
+				EquipoIncidencia f = incidenciaForm.get();
+				//f.create_usuario_id = new Long(Usuario.getUsuarioSesion());
+				//f.create_date = new Date();
+				f.save();
+
+				flash("success", "El registro se almacenó correctamente.");
+			}
+		} catch (Exception e){
+			play.Logger.error("excepcion", e);
+			flash("error", "No se ha podido almacenar el registro.");
+			return ok(crearEquipoIncidencia.render(incidenciaForm));
+		}
+
+		EquipoIncidencia incidencia = EquipoIncidencia.find.where().eq("id", incidenciaForm.get().id).findUnique();
+		Object c = verEquipoIncidencia.render(incidencia);
+		ObjectNode restJs = Json.newObject();
+		restJs.put("success", true);
+		restJs.put("nuevo", true);
+		restJs.put("html", c.toString());
+		return ok(restJs);
+	}
+
+	public static Result editar(Long id) {
+		flash().clear();
+		EquipoIncidencia incidencia = EquipoIncidencia.find.byId(id);
+		return ok(editarEquipoIncidencia.render(incidenciaForm.fill(incidencia)));
+	}
+
+	public static Result actualizar() {
+
+		Form<EquipoIncidencia> incidenciaForm = form(EquipoIncidencia.class).bindFromRequest();
+
+		try {
+			if(incidenciaForm.hasErrors()) {
+				flash("error", "Error en formulario");
+				return ok(editarEquipoIncidencia.render(incidenciaForm));
+			} else {
+				EquipoIncidencia fl = incidenciaForm.get();
+				//fl.write_usuario_id = new Long(Usuario.getUsuarioSesion());
+				//fl.write_date = new Date();
+				fl.update(fl.id);
+			}
+		} catch (Exception e){
+			play.Logger.error("excepcion", e);
+			flash("error", "No se ha podido almacenar el registro.");
+			return ok(editarEquipoIncidencia.render(incidenciaForm));
+		}
+
+		EquipoIncidencia incidencia = EquipoIncidencia.find.where().eq("id", incidenciaForm.get().id).findUnique();
+		Object c = verEquipoIncidencia.render(incidencia);
+		ObjectNode restJs = Json.newObject();
+		restJs.put("success", true);
+		restJs.put("modificar", true);
+		restJs.put("html", c.toString());
+		return ok(restJs);
+	}
+
+	public static Result eliminar(Long id) {
+		ObjectNode restJs = Json.newObject();
+
+		try {
+			EquipoIncidencia.find.byId(id).delete();
+		} catch (PersistenceException pe) {
+			play.Logger.error("excepcion", pe);
+			restJs.put("succes", false);
+		}
+
+		restJs.put("success", true);
+		return ok(restJs);
+	}
+}
