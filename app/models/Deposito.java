86904d9d885aabb4d3fae6086025ecd3ddd0e749
Marcelo Schell <mschell@proveedor.teco.com.ar>
2024-09-25 21:14:38 -0300

    add

--- app/models/Deposito.java
+++ app/models/Deposito.java
@@ -16,11 +16,11 @@ import play.data.validation.Constraints.Required;
 import play.db.ebean.Model;
 import utils.pagination.Pagination;
 
-@Entity 
+@Entity
 @Table(name = "depositos")
 public class Deposito extends Model{
 	private static final long serialVersionUID = 1L;
-	
+
 	public static final int HEARM = 1;
 	public static final int MATERNO = 2;
 	public static final int LACMI = 3;
@@ -30,49 +30,51 @@ public class Deposito extends Model{
 	public static final int LEMIS = 29;
 	public static final int BANCO_SANGRE = 30;
 	public static final int MINISTERIO_SALUD = 31;
-	
-	@Id  														 
+
+	@Id
 	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="depositos_id_seq")
 	public Long id;
 	@Required(message="Debe escribir un nombre")
 	public String nombre;
-	
+
 	public String sigla;
-	
+
 	public String direccion;
-	
+
+	public String fantasia;
+
 	public Boolean parque = false;
-	
+
 	@ManyToOne
 	@JoinColumn(name="create_usuario_id", referencedColumnName="id", insertable=false, updatable=false)
-	public Usuario create_usuario; 
+	public Usuario create_usuario;
 	@Column(name="create_usuario_id")
 	public Long create_usuario_id;
-	 
-	public Date create_date; 
-	public Date write_date; 
-	
+
+	public Date create_date;
+	public Date write_date;
+
 	@ManyToOne
 	@JoinColumn(name="write_usuario_id", referencedColumnName="id", insertable=false, updatable=false)
-	public Usuario write_usuario; 
+	public Usuario write_usuario;
 	@Column(name="write_usuario_id")
 	public Long write_usuario_id;
-	
+
 	public static Model.Finder<Long,Deposito> find = new Finder<Long,Deposito>(Long.class, Deposito.class);
-	
+
 	public List<Deposito> getDataSuggest(String input,Integer limit){
 		List<Deposito> l = find.where()
 				.ilike("nombre", "%"+input+"%")
 				.setMaxRows(limit).orderBy("nombre")
-			    .findList();  
+			    .findList();
 		return l;
 	}
-	
-	public static Pagination<Deposito> page(String nombre) {    	
+
+	public static Pagination<Deposito> page(String nombre) {
     	Pagination<Deposito> p = new Pagination<Deposito>();
     	p.setOrderDefault("DESC");
     	p.setSortByDefault("id");
-    	
+
     	p.setExpressionList(
 	    			find.where()
 	                .ilike("nombre", "%" + nombre + "%")
