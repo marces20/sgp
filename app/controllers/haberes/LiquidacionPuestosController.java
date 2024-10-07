b28696548a20b0041134fc8aa3bc658264a0a4e3
Marcelo Schell <mschell@proveedor.teco.com.ar>
2024-10-07 08:54:28 -0300

    addc control

--- app/controllers/haberes/LiquidacionPuestosController.java
+++ app/controllers/haberes/LiquidacionPuestosController.java
@@ -37,13 +37,13 @@ import views.html.haberes.liquidacionPuestos.*;
 import views.html.haberes.liquidacionPuestos.modales.modalPasarAprobado;
 
 public class LiquidacionPuestosController extends Controller {
-	
+
 	final static Form<LiquidacionPuesto> liquidacionPuestoForm = form(LiquidacionPuesto.class);
-	
+
 	public static Result URL_LISTA_LIQUIDACION_PUESTO = redirect(
 			controllers.haberes.routes.LiquidacionPuestosController.index()
 	);
-	
+
 	@CheckPermiso(key = "liquidacionPuestoIndex")
 	public static Result index() {
 		DynamicForm d = form().bindFromRequest();
@@ -66,19 +66,19 @@ public class LiquidacionPuestosController extends Controller {
 								),
 								 d, pf));
 	}
-	
+
 	@CheckPermiso(key = "liquidacionPuestoCrear")
 	public static Result crear() {
 		Form<LiquidacionPuesto> liquidacionPuestoForm = form(LiquidacionPuesto.class);
 		return ok(crearLiquidacionPuesto.render(liquidacionPuestoForm));
 	}
-	
-	
+
+
 	@CheckPermiso(key = "liquidacionPuestoCrear")
 	public static Result guardar() {
-		
+
 		Form<LiquidacionPuesto> liquidacionPuestoForm = form(LiquidacionPuesto.class).bindFromRequest();
-		
+
 		try {
 			if(liquidacionPuestoForm.hasErrors()) {
 				flash("error", "Error en formulario");
@@ -98,25 +98,25 @@ public class LiquidacionPuestosController extends Controller {
 			return badRequest(crearLiquidacionPuesto.render(liquidacionPuestoForm));
 		}
 	}
-	
+
 	@CheckPermiso(key = "liquidacionPuestoEditar")
 	public static Result editar(Long id) {
 		LiquidacionPuesto lc = Ebean.find(LiquidacionPuesto.class, id);
 		if(lc.estado_id != Estado.LIQUIDACION_PUESTOS_BORRADOR){
 			flash("error", "La liquidacion no se puede editar en este Estado. Debe cambiar su estado a borrador.");
 			return redirect(request().getHeader("referer"));
-		}										
-		
+		}
+
 		return ok(editarLiquidacionPuesto.render(liquidacionPuestoForm.fill(lc)));
 	}
-	
+
 	@CheckPermiso(key = "liquidacionPuestoEditar")
 	public static Result actualizar(){
-		
+
 		Form<LiquidacionPuesto> liquidacionPuestoForm = form(LiquidacionPuesto.class).bindFromRequest();
-		
+
 		try {
-			
+
 			if(liquidacionPuestoForm.hasErrors()) {
 				flash("error", "Error en formulario" + liquidacionPuestoForm.errors());
 				return badRequest(editarLiquidacionPuesto.render(liquidacionPuestoForm));
@@ -128,14 +128,14 @@ public class LiquidacionPuestosController extends Controller {
 				flash("success", "La liquidacion puesto se ha actualizado");
 				return redirect( controllers.haberes.routes.LiquidacionPuestosController.ver( liquidacionPuestoForm.get().id ) + UriTrack.get("&"));
 			}
-			
+
 		} catch (PersistenceException pe){
 			play.Logger.error("excepcion", pe);
 			flash("error", "No se ha podido almacenar la liquidacion puesto");
 			return badRequest(editarLiquidacionPuesto.render(liquidacionPuestoForm));
 		}
 	}
-	
+
 	@CheckPermiso(key = "liquidacionPuestoEliminar")
 	public static Result eliminar(Long id) {
 		try {
@@ -143,9 +143,9 @@ public class LiquidacionPuestosController extends Controller {
 			if(lc.estado_id != Estado.LIQUIDACION_PUESTOS_BORRADOR){
 				flash("error", "La liquidacion no se puede eliminar en este Estado. Debe cambiar su estado a borrador.");
 				return redirect(request().getHeader("referer"));
-			}										
-			
-			
+			}
+
+
 			LiquidacionPuesto.find.byId(id).delete();
 			flash("success", "Se ha eliminado la liquidacion puesto");
 			return redirect( UriTrack.decode() );
@@ -156,7 +156,7 @@ public class LiquidacionPuestosController extends Controller {
 
 		return redirect(request().getHeader("referer"));
 	}
-	
+
 	@CheckPermiso(key = "liquidacionPuestoVer")
 	public static Result ver(Long id) throws IOException {
 		LiquidacionPuesto lc = LiquidacionPuesto.find
@@ -168,37 +168,37 @@ public class LiquidacionPuestosController extends Controller {
 			flash("error", "No se encuentra la liquidacion puesto.");
 			return URL_LISTA_LIQUIDACION_PUESTO;
 		}
-		
+
 		return ok(verLiquidacionPuesto.render(liquidacionPuestoForm.fill(lc),lc, new PaginadorFicha(UriTrack.code())));
 	}
-	
+
 	public static Result suggestLiquidacionPuesto(String input) {
-		 
+
 		ObjectNode rpta = Json.newObject();
 	    ArrayNode liquidacionPuesto = rpta.arrayNode();
-	    
+
 	    LiquidacionPuesto lc = new LiquidacionPuesto();
-		 
+
 		for(LiquidacionPuesto a : lc.getDataSuggest(input, 25)){
 			ObjectNode restJs = Json.newObject();
 	        restJs.put("id", a.id);
 	        restJs.put("value",a.liquidacionMes.nro_liquidacion_parque);
 	        liquidacionPuesto.add(restJs);
 		}
-		
+
 		ObjectNode response = Json.newObject();
 		response.put("results", liquidacionPuesto);
-		 
+
 		return ok(response);
 	}
-	
+
 	public static Result get(int id){
 		LiquidacionPuesto lc = LiquidacionPuesto.find.select("id, nombre").where().eq("id", id).findUnique();
-		
+
 		ObjectNode obj = Json.newObject();
 	    ArrayNode nodo = obj.arrayNode();
 		ObjectNode restJs = Json.newObject();
-		
+
 		if(lc == null) {
 			restJs.put("success", false);
 			restJs.put("message", "No se encuentra la liquidacion");
@@ -210,18 +210,18 @@ public class LiquidacionPuestosController extends Controller {
 		nodo.add(restJs);
 		return ok(restJs);
 	}
-	
+
 	public static Result modalBuscar() {
     	Pagination<LiquidacionPuesto> p = new Pagination<LiquidacionPuesto>();
     	p.setOrderDefault("DESC");
-    	p.setSortByDefault("id");	
+    	p.setSortByDefault("id");
     	p.setExpressionList(LiquidacionPuesto.find.where().ilike("nro_liq_parque", "%" + RequestVar.get("nro_liq_parque") + "%"));
 		return ok(modalBusquedaLiquidacionPuesto.render(p, form().bindFromRequest()) );
 	}
-	
+
 	public static Result cambiarEstado(Long idLiquidacionPuesto, Long idEstado) throws IOException{
 		Boolean permiso = false;
-		
+
 		LiquidacionPuesto lc = LiquidacionPuesto.find.where().eq("id", idLiquidacionPuesto).findUnique();
 		if(lc.liquidacionMes.estado_id != Estado.LIQUIDACION_MES_APROBADO) {
 			switch ( idEstado.intValue() ) {
@@ -236,34 +236,34 @@ public class LiquidacionPuestosController extends Controller {
 					  return ok(sinPermiso.render(request().getHeader("referer")));
 				  }
 		    	  pasarPreaprobado(idLiquidacionPuesto);
-		    	  break;       
+		    	  break;
 		      case Estado.LIQUIDACION_PUESTOS_APROBADO:
 		    	  if(!Permiso.check("liquidacionPuestoPasarAAprobado")) {
 					  return ok(sinPermiso.render(request().getHeader("referer")));
 				  }
-		    	  pasarAprobado(idLiquidacionPuesto); 
+		    	  pasarAprobado(idLiquidacionPuesto);
 		    	  break;
 		      case Estado.LIQUIDACION_PUESTOS_CANCELADO:
 		    	  if(!Permiso.check("liquidacionPuestoPasarACancelado")) {
 					  return ok(sinPermiso.render(request().getHeader("referer")));
 				  }
-		    	  pasarCancelado(idLiquidacionPuesto);   
+		    	  pasarCancelado(idLiquidacionPuesto);
 		          break;
 		      default:
 		           break;
-		      } 
+		      }
 		}else {
 			flash("error", "No se puede cambiar de estado las liquidaciones cuando la liquidacion esta en estado APROBADA.");
 		}
-	    
+
 		return redirect(controllers.haberes.routes.LiquidacionPuestosController.ver(idLiquidacionPuesto)+ UriTrack.get("&"));
 	}
-	
+
 	public static void pasarEnBorrador(Long idLiquidacionPuesto){
-		
+
 		LiquidacionPuesto lp = Ebean.find(LiquidacionPuesto.class).select("id, estado_id").setId(idLiquidacionPuesto).findUnique();
-		
-		if(lp != null){			
+
+		if(lp != null){
 			lp.estado_id = new Long(Estado.LIQUIDACION_PUESTOS_BORRADOR);
 			lp.save();
 			flash("success", "Operación exitosa. Estado actual: Borrador");
@@ -271,12 +271,12 @@ public class LiquidacionPuestosController extends Controller {
 			flash("error", "Parámetros incorrectos");
 		}
 	}
-	
+
 	public static void pasarPreaprobado(Long idLiquidacionPuesto){
-		
+
 		LiquidacionPuesto lp = Ebean.find(LiquidacionPuesto.class).select("id, estado_id").setId(idLiquidacionPuesto).findUnique();
-		
-		if(lp != null){			
+
+		if(lp != null){
 			lp.estado_id = new Long(Estado.LIQUIDACION_PUESTOS_PREAPROBADO);
 			lp.save();
 			flash("success", "Operación exitosa. Estado actual: Preaprobado");
@@ -284,14 +284,17 @@ public class LiquidacionPuestosController extends Controller {
 			flash("error", "Parámetros incorrectos");
 		}
 	}
-	
+
 	public static void pasarAprobado(Long idLiquidacionPuesto){
-		
+
 		LiquidacionPuesto lp = Ebean.find(LiquidacionPuesto.class).select("id, estado_id").setId(idLiquidacionPuesto).findUnique();
-		
+
 		List<LiquidacionPuesto> lcx = LiquidacionPuesto.find.where()
 				   .eq("id",idLiquidacionPuesto)
+				   .disjunction()
 				   .isNotNull("puestoLaboral.legajo.agente.fbaja")
+				   .ne("puestoLaboral.estado_id",Estado.PUESTO_LABORAL_CONTROLADO)
+				   .endJunction()
 				   .findList();
 
 		if(lcx.size() > 0) {
@@ -299,11 +302,11 @@ public class LiquidacionPuestosController extends Controller {
 			for(LiquidacionPuesto lcz : lcx) {
 				f += lcz.puestoLaboral.legajo.agente.apellido+"<br>";
 			}
-			flash("error", "No se puede aprobar liquidaciones con fecha de baja en agentes.<br>"+f);
-			 
+			flash("error", "No se puede aprobar liquidaciones con fecha de baja en agentes o el puesto laboral no está aprobado.<br>"+f);
+
 		}else {
-		
-			if(lp != null){			
+
+			if(lp != null){
 				lp.estado_id = new Long(Estado.LIQUIDACION_PUESTOS_APROBADO);
 				lp.save();
 				flash("success", "Operación exitosa. Estado actual: Aprobado");
@@ -312,12 +315,12 @@ public class LiquidacionPuestosController extends Controller {
 			}
 		}
 	}
-	
+
 	public static void pasarCancelado(Long idLiquidacionPuesto){
-		
+
 		LiquidacionPuesto lp = Ebean.find(LiquidacionPuesto.class).select("id, estado_id").setId(idLiquidacionPuesto).findUnique();
-		
-		if(lp != null){			
+
+		if(lp != null){
 			lp.estado_id = new Long(Estado.LIQUIDACION_PUESTOS_CANCELADO);
 			lp.save();
 			flash("success", "Operación exitosa. Estado actual: Cancelado");
@@ -325,10 +328,10 @@ public class LiquidacionPuestosController extends Controller {
 			flash("error", "Parámetros incorrectos");
 		}
 	}
-	
+
 	@CheckPermiso(key = "liquidacionMesPreliquidar")
 	public static Result preLiquidar(Long idPuestoLaboral,Long idLiquidacionMes,Long idLiquidacionPuesto) throws SQLException{
-		
+
 		boolean c = LiquidacionMes.controlLiquidacionesActivas();
 		LiquidacionPuesto lp = LiquidacionPuesto.find.where().eq("puesto_laboral_id", idPuestoLaboral).eq("liquidacion_mes_id", idLiquidacionMes).findUnique();
 		Long idNew = lp.id;
@@ -336,7 +339,7 @@ public class LiquidacionPuestosController extends Controller {
 			if(!c) {
 				int idControl = new LiquidacionMes().controlLiquidacion(idPuestoLaboral,idLiquidacionMes);
 				idNew  = new LiquidacionMes().preliquidar(idPuestoLaboral,idLiquidacionMes,idControl);
-				
+
 			}else {
 				flash("error", "HAY PUESTO LIQUIDANDOSE SOLO SE PUEDE LIQUIDAR UN PUESTO A LA VEZ. ESPERE Y VUELVA A INTENTARLO. ");
 			}
@@ -345,10 +348,10 @@ public class LiquidacionPuestosController extends Controller {
 			Logger.error("Error duplicar: "+e);
 			return redirect(controllers.haberes.routes.LiquidacionPuestosController.ver(lp.id)+ UriTrack.get("&"));
         }
-		
+
 		Logger.debug("zzzzzzzzzzzzzzzzz "+idNew);
-		
+
 		return redirect(controllers.haberes.routes.LiquidacionPuestosController.ver(idNew)+ UriTrack.get("&"));
 	}
-	
+
 }
