46b561100265646b39b92fd423bd6662aa3fcb2d
Marcelo Schell <mschell@proveedor.teco.com.ar>
2024-11-11 21:51:35 -0300

    add

--- app/models/ClienteTipo.java
+++ app/models/ClienteTipo.java
@@ -14,11 +14,11 @@ import play.db.ebean.Model.Finder;
 import play.mvc.Result;
 import views.html.administracion.provincias.optionsListProvincia;
 
-@Entity 
+@Entity
 @Table(name = "clientes_tipos")
 public class ClienteTipo extends Model {
 	private static final long serialVersionUID = 1L;
-	
+
 	public static Long OBRAS_SOCIALES = (long)1;
 	public static Long EXTRANJEROS = (long)2;
 	public static Long SEGUROS = (long)3;
@@ -29,21 +29,23 @@ public class ClienteTipo extends Model {
 	public static Long PARTICULARES_ROBOTICA = (long)8;
 	public static Long ART = (long)9;
 	public static Long OTROS = (long)10;
-	
-	
-	
-	
 
-	
-	@Id  
+
+
+
+
+
+	@Id
 	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="clientes_tipos_id_seq")
 	public Long id;
 
 	@Required(message="Nombre requerido")
-	public String nombre;	
-	
+	public String nombre;
+
+	public Integer orden;
+
 	public static Finder<Long,ClienteTipo> find = new Finder<Long,ClienteTipo>(Long.class, ClienteTipo.class);
-	
+
 }
 
 
