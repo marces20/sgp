903d4d6c728d8a4e4a1c3aeccfb461eafc3cd3b3
Marcelo Schell <mschell@proveedor.teco.com.ar>
2024-11-21 16:43:56 -0300

    add

--- app/models/recupero/RecuperoFactura.java
+++ app/models/recupero/RecuperoFactura.java
@@ -355,7 +355,7 @@ public class RecuperoFactura extends Model {
 			if(Usuario.getUsurioSesion().organigrama != null && Usuario.getUsurioSesion().organigrama.deposito != null){
 				//RecuperoPlanilla rp = RecuperoPlanilla.find.byId(planilla_id.longValue());
 
-				if(Usuario.getUsurioSesion().organigrama.deposito_id.compareTo(this.puntoVenta.deposito_id) != 0) {
+				if(this.puntoVenta.deposito_id != null && Usuario.getUsurioSesion().organigrama.deposito_id.compareTo(this.puntoVenta.deposito_id) != 0) {
 					r = false;
 				}
 			}else {
