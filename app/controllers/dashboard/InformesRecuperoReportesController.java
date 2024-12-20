aa6eaddab1f44a5f75a41893a54cf5eccc015d0f
Marcelo Schell <mschell@proveedor.teco.com.ar>
2024-12-20 09:31:16 -0300

    add

--- app/controllers/dashboard/InformesRecuperoReportesController.java
+++ app/controllers/dashboard/InformesRecuperoReportesController.java
@@ -31,24 +31,24 @@ import controllers.Secured;
 
 @Security.Authenticated(Secured.class)
 public class InformesRecuperoReportesController extends Controller {
-	
-	public static Result deudasPorTipoDeCliente(Long idTipoCliente) {
-		
-		 
+
+	public static Result deudasPorTipoDeCliente(Long idTipoCliente,String fecha_desde,String fecha_hasta) {
+
+
 		String dirTemp = System.getProperty("java.io.tmpdir");
 		Date ahora = new Date();
-		
+
 		File archivo = new File(dirTemp+"/INFORME-DEUDAS-RESUMEN"+utils.DateUtils.formatDate(ahora, "dd-MM-yyyy")+".xls");
 		try {
 			if(archivo.exists()) archivo.delete();
 			archivo.createNewFile();
-			
+
 			Workbook libro = new HSSFWorkbook();
 			FileOutputStream archivoTmp = new FileOutputStream(archivo);
-			Sheet hoja = getDeudasPorTipoDeClienteSheet(libro,idTipoCliente);
-			
-			
-			libro.write(archivoTmp); 
+			Sheet hoja = getDeudasPorTipoDeClienteSheet(libro,idTipoCliente,fecha_desde,fecha_hasta);
+
+
+			libro.write(archivoTmp);
 			return ok(archivo);
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -58,43 +58,44 @@ public class InformesRecuperoReportesController extends Controller {
 			e.printStackTrace();
 			flash("error", "No se pudo generar el reporte");
 			return ok(index.render());
-		}	
+		}
 	}
-	
-	public static Sheet getDeudasPorTipoDeClienteSheet(Workbook libro,Long idTipoCliente){
-		
+
+	public static Sheet getDeudasPorTipoDeClienteSheet(Workbook libro,Long idTipoCliente,String fecha_desde,String fecha_hasta){
+
 		List<SqlRow> deudasPorPaciente = new ArrayList<SqlRow>();
 		List<SqlRow> deudasPorExtrajero = new ArrayList<SqlRow>();
 		List<SqlRow> deudasPorObrasSociales = new ArrayList<SqlRow>();
 		List<SqlRow> deudasPorArt = new ArrayList<SqlRow>();
 		List<SqlRow> deudasPorSeguro = new ArrayList<SqlRow>();
-		
+
 		if(idTipoCliente.equals(ClienteTipo.PACIENTES) || idTipoCliente.equals(new Long(-1))){
-			deudasPorPaciente = InformeTotal.getDeudaPorTipoDeCliente(ClienteTipo.PACIENTES);
+			deudasPorPaciente = InformeTotal.getDeudaPorTipoDeCliente(ClienteTipo.PACIENTES,fecha_desde,fecha_hasta);
 		}
 		if(idTipoCliente.equals(ClienteTipo.EXTRANJEROS) || idTipoCliente.equals(new Long(-1))){
-			deudasPorExtrajero = InformeTotal.getDeudaPorTipoDeCliente(ClienteTipo.EXTRANJEROS);
+			deudasPorExtrajero = InformeTotal.getDeudaPorTipoDeCliente(ClienteTipo.EXTRANJEROS,fecha_desde,fecha_hasta);
 		}
 		if(idTipoCliente.equals(ClienteTipo.OBRAS_SOCIALES) || idTipoCliente.equals(new Long(-1))){
-			deudasPorObrasSociales = InformeTotal.getDeudaPorTipoDeCliente(ClienteTipo.OBRAS_SOCIALES);
+			deudasPorObrasSociales = InformeTotal.getDeudaPorTipoDeCliente(ClienteTipo.OBRAS_SOCIALES,fecha_desde,fecha_hasta);
 		}
 		if(idTipoCliente.equals(ClienteTipo.ART) || idTipoCliente.equals(new Long(-1))){
-			deudasPorArt = InformeTotal.getDeudaPorTipoDeCliente(ClienteTipo.ART);
+			deudasPorArt = InformeTotal.getDeudaPorTipoDeCliente(ClienteTipo.ART,fecha_desde,fecha_hasta);
 		}
-		
+
 		if(idTipoCliente.equals(ClienteTipo.SEGUROS) || idTipoCliente.equals(new Long(-1))){
-			deudasPorSeguro = InformeTotal.getDeudaPorTipoDeCliente(ClienteTipo.SEGUROS);
+			deudasPorSeguro = InformeTotal.getDeudaPorTipoDeCliente(ClienteTipo.SEGUROS,fecha_desde,fecha_hasta);
 		}
-		
+
 		Sheet hoja = libro.createSheet("RESUMEN");
-		
-		hoja.setColumnWidth(0, 15000);
+
+		hoja.setColumnWidth(0, 18000);
 		hoja.setColumnWidth(1, 5000);
 		hoja.setColumnWidth(2, 5000);
 		hoja.setColumnWidth(3, 5000);
-		 
+		hoja.setColumnWidth(4, 5000);
+
 		hoja.setDefaultRowHeight( (short) 400);
-		
+
 		int x = 0;
 		if(deudasPorPaciente.size() > 0){
 			x = getListadoDeudasPorTipoDeCliente(x,"PACIENTES",libro,hoja,deudasPorPaciente);
@@ -111,105 +112,120 @@ public class InformesRecuperoReportesController extends Controller {
 		if(deudasPorArt.size() > 0){
 			x = getListadoDeudasPorTipoDeCliente(x,"ART",libro,hoja,deudasPorArt);//servicios
 		}
-		
+
 		x= 0;
-		
+
 		return hoja;
 
 	}
-	
+
 	public static int getListadoDeudasPorTipoDeCliente(int x,String tipoCliente,Workbook libro,Sheet hoja,List<SqlRow> lista){
 		ReportesExcelsUtils reu = new ReportesExcelsUtils();
 		CellStyle estiloMoneda = reu.getEstiloMoneda(libro);
 		CellStyle comun = reu.getEstiloComun(libro);
 		CellStyle cabeceraPrincipal = reu.getCabecera(libro,14);
 		CellStyle cabecera = reu.getCabecera(libro,10);
-		
+
 		Row fila = hoja.createRow(x);
-		
+
 		Cell celda0 = fila.createCell(0);
 		celda0.setCellValue(tipoCliente);
 		celda0.setCellStyle(cabeceraPrincipal);
-		hoja.addMergedRegion(new  CellRangeAddress(x,x,0,3));
+		hoja.addMergedRegion(new  CellRangeAddress(x,x,0,4));
 		x++;
-		
+
 		fila = hoja.createRow(x);
-		
+
 		celda0 = fila.createCell(0);
 		celda0.setCellValue("FINANCIADOR-PACIENTE");
 		celda0.setCellStyle(cabecera);
+
 		celda0 = fila.createCell(1);
+		celda0.setCellValue("CUIT");
+		celda0.setCellStyle(cabecera);
+
+		celda0 = fila.createCell(2);
 		celda0.setCellValue("IMPORTE");
 		celda0.setCellStyle(cabecera);
-		celda0 = fila.createCell(2);
+
+		celda0 = fila.createCell(3);
 		celda0.setCellValue("DEBITOS");
 		celda0.setCellStyle(cabecera);
-		celda0 = fila.createCell(3);
-		celda0.setCellValue("A COBRAR");
+
+		celda0 = fila.createCell(4);
+		celda0.setCellValue("SALDO");
 		celda0.setCellStyle(cabecera);
-		
+
 		x++;
-		
+
 		BigDecimal mtfa = new BigDecimal(0);
 		BigDecimal mtpa = new BigDecimal(0);
 		BigDecimal mtde = new BigDecimal(0);
 		boolean cargar = false;
 		boolean xx = false;
 		boolean ss = true;
-					
+
 		for(SqlRow s : lista){
 	    	fila = hoja.createRow(x);
 	    	celda0 = fila.createCell(0);
 			celda0.setCellValue(s.getString("cliente_nombre"));
 			celda0.setCellStyle(comun);
-					 
+
 			celda0 = fila.createCell(1);
+			celda0.setCellValue(s.getString("cuit"));
+			celda0.setCellStyle(comun);
+
+			celda0 = fila.createCell(2);
 			celda0.setCellValue(s.getBigDecimal("total_factura").doubleValue());
 			celda0.setCellStyle(estiloMoneda);
-			
-			celda0 = fila.createCell(2);
+
+			celda0 = fila.createCell(3);
 			celda0.setCellValue(s.getBigDecimal("total_pago").doubleValue());
 			celda0.setCellStyle(estiloMoneda);
-			
-			celda0 = fila.createCell(3);
+
+			celda0 = fila.createCell(4);
 			celda0.setCellValue(s.getBigDecimal("total_deuda").doubleValue());
 			celda0.setCellStyle(estiloMoneda);
-			
+
 			x++;
-			
+
 			mtfa= mtfa.add(s.getBigDecimal("total_factura"));
 			mtpa= mtpa.add(s.getBigDecimal("total_pago"));
 			mtde= mtde.add(s.getBigDecimal("total_deuda"));
 			//nombreProveedor = s.getString("nombre_proveedor");
 	    }
-					 
+
 		    fila = hoja.createRow(x);
 		    celda0 = fila.createCell(0);
+			celda0.setCellValue("");
+			celda0.setCellStyle(comun);
+
+			celda0 = fila.createCell(1);
 			celda0.setCellValue("TOTAL");
 			celda0.setCellStyle(comun);
 			//hoja.addMergedRegion(new  CellRangeAddress(x,x,0,1));
-			
-			celda0 = fila.createCell(1);
+
+			celda0 = fila.createCell(2);
 			celda0.setCellValue(mtfa.doubleValue());
 			celda0.setCellStyle(estiloMoneda);
-			celda0 = fila.createCell(2);
+			celda0 = fila.createCell(3);
 			celda0.setCellValue(mtpa.doubleValue());
 			celda0.setCellStyle(estiloMoneda);
-			celda0 = fila.createCell(3);
+			celda0 = fila.createCell(4);
 			celda0.setCellValue(mtde.doubleValue());
 			celda0.setCellStyle(estiloMoneda);
-			
+
 			/*celda0 = fila.createCell(4);
 			celda0.setCellValue("");
 			celda0.setCellStyle(comun);
 			hoja.addMergedRegion(new  CellRangeAddress(x,x,4,5));*/
 			x++;
 			x++;
-	     
-						
-						
-		 
-		
+
+
+
+
+
 		return x;
 	}
 }
