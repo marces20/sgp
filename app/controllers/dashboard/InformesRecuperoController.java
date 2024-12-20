30ab704affc55c733cb72f758f09eeb387454df9
Marcelo Schell <mschell@proveedor.teco.com.ar>
2024-12-20 09:31:06 -0300

    add

--- app/controllers/dashboard/InformesRecuperoController.java
+++ app/controllers/dashboard/InformesRecuperoController.java
@@ -1,5 +1,7 @@
 package controllers.dashboard;
 
+import static play.data.Form.form;
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -11,9 +13,11 @@ import models.recupero.InformeTotal;
 
 import com.avaje.ebean.SqlRow;
 
+import play.data.DynamicForm;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Security;
+import utils.RequestVar;
 import views.html.dashboard.informesRecupero.*;
 import controllers.Secured;
 
@@ -25,6 +29,7 @@ public class InformesRecuperoController extends Controller {
 	}
 
 	public static Result deudasPorTipoDeCliente(Long idTipoCliente) {
+
 		List<SqlRow> deudasPorPaciente = new ArrayList<SqlRow>();
 		List<SqlRow> deudasPorExtrajero = new ArrayList<SqlRow>();
 		List<SqlRow> deudasPorObrasSociales = new ArrayList<SqlRow>();
@@ -33,29 +38,33 @@ public class InformesRecuperoController extends Controller {
 		Map<Long, Integer> totalPagosObra = new HashMap<>();
 		Map<Long, Date> pagoLastDateObra = new HashMap<>();
 
+
+		DynamicForm d = form().bindFromRequest();
+
+
 		if(idTipoCliente.equals(ClienteTipo.PACIENTES) || idTipoCliente.equals(new Long(-1))){
-			deudasPorPaciente = InformeTotal.getDeudaPorTipoDeCliente(ClienteTipo.PACIENTES);
+			deudasPorPaciente = InformeTotal.getDeudaPorTipoDeCliente(ClienteTipo.PACIENTES,RequestVar.get("fecha_desde"),RequestVar.get("fecha_hasta"));
 		}
 		if(idTipoCliente.equals(ClienteTipo.EXTRANJEROS) || idTipoCliente.equals(new Long(-1))){
-			deudasPorExtrajero = InformeTotal.getDeudaPorTipoDeCliente(ClienteTipo.EXTRANJEROS);
+			deudasPorExtrajero = InformeTotal.getDeudaPorTipoDeCliente(ClienteTipo.EXTRANJEROS,RequestVar.get("fecha_desde"),RequestVar.get("fecha_hasta"));
 		}
 		if(idTipoCliente.equals(ClienteTipo.OBRAS_SOCIALES) || idTipoCliente.equals(new Long(-1))){
-			deudasPorObrasSociales = InformeTotal.getDeudaPorTipoDeCliente(ClienteTipo.OBRAS_SOCIALES);
+			deudasPorObrasSociales = InformeTotal.getDeudaPorTipoDeCliente(ClienteTipo.OBRAS_SOCIALES,RequestVar.get("fecha_desde"),RequestVar.get("fecha_hasta"));
 			totalPagosObra = InformeTotal.getPagoClienteConDeudaPorTipoDeCliente(ClienteTipo.OBRAS_SOCIALES);
 			pagoLastDateObra = InformeTotal.getPagoLastDateClienteConDeudaPorTipoDeCliente(ClienteTipo.OBRAS_SOCIALES);
 
 		}
 		if(idTipoCliente.equals(ClienteTipo.ART) || idTipoCliente.equals(new Long(-1))){
-			deudasPorArt = InformeTotal.getDeudaPorTipoDeCliente(ClienteTipo.ART);
+			deudasPorArt = InformeTotal.getDeudaPorTipoDeCliente(ClienteTipo.ART,RequestVar.get("fecha_desde"),RequestVar.get("fecha_hasta"));
 		}
 
 		if(idTipoCliente.equals(ClienteTipo.SEGUROS) || idTipoCliente.equals(new Long(-1))){
-			deudasPorSeguro = InformeTotal.getDeudaPorTipoDeCliente(ClienteTipo.SEGUROS);
+			deudasPorSeguro = InformeTotal.getDeudaPorTipoDeCliente(ClienteTipo.SEGUROS,RequestVar.get("fecha_desde"),RequestVar.get("fecha_hasta"));
 		}
 
 
 		return ok(deudasPorTipoDeCliente.render(idTipoCliente,
 												deudasPorPaciente,deudasPorExtrajero,deudasPorObrasSociales,deudasPorArt,deudasPorSeguro,
-												totalPagosObra,pagoLastDateObra));
+												totalPagosObra,pagoLastDateObra,d,RequestVar.get("fecha_desde"),RequestVar.get("fecha_hasta")));
 	}
 }
