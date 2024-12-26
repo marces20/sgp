7aab9ce3f911cc61157495f1d64a42e74a18aeee
Marcelo Schell <mschell@proveedor.teco.com.ar>
2024-12-26 15:27:21 -0300

    aff

--- app/controllers/expediente/ExpedientesController.java
+++ app/controllers/expediente/ExpedientesController.java
@@ -80,7 +80,7 @@ import fr.opensagres.xdocreport.template.formatter.FieldsMetadata;
 @Security.Authenticated(Secured.class)
 public class ExpedientesController extends Controller {
 
-	final static Long ID_EJERCICIO_PREDETERMINADO = (long) 15;
+	final static Long ID_EJERCICIO_PREDETERMINADO = (long) 16;
 	final static Form<Expediente> expedienteForm = form(Expediente.class);
 
 	public static Result URL_LISTA_IEXPEDIENTE = redirect(
