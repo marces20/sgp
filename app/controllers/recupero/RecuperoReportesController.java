1e9954b67cc9ec39c5a2816c68aee68402792e28
Marcelo Schell <mschell@proveedor.teco.com.ar>
2025-02-04 12:51:40 -0300

    add

--- app/controllers/recupero/RecuperoReportesController.java
+++ app/controllers/recupero/RecuperoReportesController.java
@@ -2978,7 +2978,7 @@ order by nc.numero
 	    String recupero_tipo_pago = (rf.recupero_tipo_pago_id != null)?rf.recuperoTipoPago.nombre:"Contado";
 
 	    //if(recupero_tipo_pago.compareToIgnoreCase("cuenta corriente") ==  0 && rf.puntoVenta.deposito_id.intValue() == Deposito.LACMI) {
-	    if(rf.cliente_id.compareTo(new Long(11081)) ==  0) {
+	    if(rf.cliente_id.compareTo(new Long(7092)) ==  0) {
 	    	recupero_tipo_pago = "Contado";
 	    }else if(recupero_tipo_pago.compareToIgnoreCase("cuenta corriente") ==  0) {
 	    	recupero_tipo_pago = "Otra";
