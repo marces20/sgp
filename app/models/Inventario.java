0e01df70553c997f3cf0609a09fe47595ee72a11
Marcelo Schell <mschell@proveedor.teco.com.ar>
2025-04-03 12:30:57 -0300

    add

--- app/models/Inventario.java
+++ app/models/Inventario.java
@@ -25,7 +25,7 @@ public class Inventario extends Model {
 
 	private static final long serialVersionUID = 1L;
 
-	@Id  
+	@Id
 	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="inventario_id_seq")
 	public Long id;
 
@@ -33,60 +33,68 @@ public class Inventario extends Model {
 	public Integer numero;
 
 	public BigDecimal division;
-	
+
 	public Integer remito_id;
 	@OneToOne
 	@JoinColumn(name="remito_id", referencedColumnName="id", insertable=false, updatable=false)
 	public Remito remito;
-	
+
 	@ManyToOne
 	@JoinColumn(name="producto_id", referencedColumnName="id", insertable=false, updatable=false)
 	public  Producto producto;
 	@Required(message="Requiere producto")
 	public Long producto_id;
-	
+
 	@ManyToOne
 	@JoinColumn(name="nomenclador_inventario_id", referencedColumnName="id", insertable=false, updatable=false)
 	public InventarioPrefijo prefijo;
 	@Required(message="Requiere un prefijo")
 	public Long nomenclador_inventario_id;
 
-	
+
 	public static Model.Finder<Long,Inventario> find = new Finder<Long,Inventario>(Long.class, Inventario.class);
-	
-	public static Pagination<Inventario> page(String prefijo, String numero, String remito, String proveedor, String producto_id) {    	
+
+	public static Pagination<Inventario> page(String prefijo, String numero, String remito, String proveedor, String producto_id,String expediente_id) {
     	Pagination<Inventario> p = new Pagination<Inventario>();
     	p.setOrderDefault("DESC");
     	p.setSortByDefault("numero");
-    	
-    	ExpressionList<Inventario> e = find.where();
-    	
+
+    	ExpressionList<Inventario> e = find
+    			 .fetch("remito.recepcion.ordenProvision.ordenCompra", "proveedor_id,expediente_id")
+    			 .fetch("remito.recepcion.ordenProvision.ordenCompra.expediente")
+    			 .where();
+
+
     	if(!prefijo.isEmpty()) {
     		e.eq("prefijo_inventario_id", Integer.parseInt(prefijo));
     	}
-    	
+
     	if(!numero.isEmpty()) {
     		e.eq("numero", Integer.parseInt(numero));
     	}
-    	
+
     	if(!remito.isEmpty()) {
     		e.eq("remito.numero", remito );
     	}
-    	
+
     	if(!proveedor.isEmpty()) {
     		e.eq("remito.recepcion.ordenProvision.ordenCompra.proveedor_id", Integer.parseInt(proveedor) );
     	}
-    	
+
+    	if(!expediente_id.isEmpty()) {
+    		e.eq("remito.recepcion.ordenProvision.ordenCompra.expediente_id", Integer.parseInt(expediente_id) );
+    	}
+
     	if(!producto_id.isEmpty()) {
     		e.eq("producto_id", Integer.parseInt(producto_id) );
     	}
-    	
+
     	p.setExpressionList(e);
     	return p;
     }
-	
-	
-	
+
+
+
 	/*
 select p.nombre, i.numero, pi.nombre from remitos r
 inner join remitos_lineas rl on r.id = rl.remito_id
