6c80a024c722d7538b72f3cdb6b8e64d05ca64bc
Marcelo Schell <mschell@proveedor.teco.com.ar>
2025-04-03 19:16:10 -0300

    add

--- app/models/InstitucionExterna.java
+++ app/models/InstitucionExterna.java
@@ -1,5 +1,7 @@
 package models;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -8,20 +10,40 @@ import javax.persistence.Table;
 
 import play.data.validation.Constraints.Required;
 import play.db.ebean.Model;
+import utils.pagination.Pagination;
 
-@Entity 
+@Entity
 @Table(name = "instituciones_externas")
 public class InstitucionExterna extends Model{
-	
+
 private static final long serialVersionUID = 1L;
-	
-	@Id  
+
+	@Id
 	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="instituciones_externas_id_seq")
 	public Long id;
 	@Required(message="Debe escribir un apellido")
 	public String nombre;
-	
+
 	public static Model.Finder<Long,InstitucionExterna> find = new Model.Finder<Long,InstitucionExterna>(Long.class, InstitucionExterna.class);
 
+	public List<InstitucionExterna> getDataSuggest(String input,Integer limit){
+		List<InstitucionExterna> l = find.where()
+				.ilike("nombre", "%"+input+"%")
+				.setMaxRows(limit).orderBy("nombre")
+			    .findList();
+		return l;
+	}
+
+	public static Pagination<InstitucionExterna> page(String nombre) {
+    	Pagination<InstitucionExterna> p = new Pagination<InstitucionExterna>();
+    	p.setOrderDefault("ASC");
+    	p.setSortByDefault("nombre");
+
+    	p.setExpressionList(
+    			 find.where()
+                .ilike("nombre", "%" + nombre + "%")
+	            );
+    	return p;
+    }
 
 }
--- app/models/Solicitud.java
+++ app/models/Solicitud.java
@@ -34,173 +34,190 @@ import com.avaje.ebean.ExpressionList;
 import com.avaje.ebean.SqlUpdate;
 import com.avaje.ebean.annotation.Formula;
 
-@Entity 
+@Entity
 @Table(name = "solicitudes")
 public class Solicitud extends Model {
 	private static final long serialVersionUID = 1L;
-	
-	@Id  														 
+
+	@Id
 	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="solicitud_id_seq")
 	public Long id;
-	
+
 	public String origen;
 	public String descripcion;
-	
+
 	@ManyToOne
 	@JoinColumn(name="deposito_id", referencedColumnName="id", insertable=false, updatable=false)
 	public Deposito deposito;
 	@Required(message="Institucion requerida")
 	public Integer deposito_id;
-	
+
 	@Required(message="Debe escribir una referencia")
 	public String referencia;
 	public String dirigida;
-	
+
 	@ManyToOne
 	@JoinColumn(name="expediente_id", referencedColumnName="id", insertable=false, updatable=false)
 	public Expediente expediente;
 	public Integer expediente_id;
-	
+
 	@OneToMany
 	public List<SolicitudLinea> lineas;
-	
+
 	@OneToMany
 	public List<SolicitudLineaAjuste> lineasAjustes;
-	
+
 	@Formats .DateTime(pattern="dd/MM/yyyy")
 	public Date fecha_imputacion;
 	public Boolean contrato_personal = false;
 	public String prioridad;
-	
+
 	@ManyToOne
 	@JoinColumn(name="cliente_id", referencedColumnName="id", insertable=false, updatable=false)
 	public Cliente cliente;
 	public Integer cliente_id;
-	
+
 	public Boolean profe;
-	
+
 	@ManyToOne
 	@JoinColumn(name="tipo_cuenta_id", referencedColumnName="id", insertable=false, updatable=false)
 	public TipoCuenta tipoCuenta;
 	@Required(message="Tipo Cuenta requerido")
 	public Long tipo_cuenta_id;
-	
+
 	public Boolean recuperable = false;
-	
+
 	public Boolean repo_stock = false;
-	
+
 	public Boolean entregado = false;
-	
+
 	@ManyToOne
 	@JoinColumn(name="departamento_id", referencedColumnName="id", insertable=false, updatable=false)
 	public Departamento departamento;
 	@Required(message="Departamento requerido")
 	public Integer departamento_id;
-	
+
 	@ManyToOne
 	@JoinColumn(name="state_id", referencedColumnName="id", insertable=false, updatable=false)
 	public Estado estado;
 
 	@Column(name="state_id")
 	public Long estado_id;
-	
+
 	@ManyToOne
 	public Agente solicitante;
-	
+
 	@ManyToOne
 	@JoinColumn(name="medico_id", referencedColumnName="id", insertable=false, updatable=false)
 	public Agente medico;
 	public Integer medico_id;
-	
+
 	public String creacion_orden_compra;
-	
+
 	public String referencia_sigpar;
-	
+
 	public String pedido_rismi_id;
 
 	@Formats .DateTime(pattern="dd/MM/yyyy")
 	public Date date_end;
-	
+
 	@Formats .DateTime(pattern="dd/MM/yyyy")
 	public Date date_start;
-	
+
+	public String diagnostico;
+
+	public Boolean ips;
+
+	@ManyToOne
+	@JoinColumn(name="ida_institucion_externa_id", referencedColumnName="id", insertable=false, updatable=false)
+	public InstitucionExterna idaInstitucionExterna;
+	public Long ida_institucion_externa_id;
+
+	@ManyToOne
+	@JoinColumn(name="vuelta_institucion_externa_id", referencedColumnName="id", insertable=false, updatable=false)
+	public InstitucionExterna vueltaInstitucionExterna;
+	public Long vuelta_institucion_externa_id;
+
+	@Formats .DateTime(pattern="dd/MM/yyyy")
+	public Date fecha_viaje;
+
 	@Formula(select = "_b${ta}.total", join = "left outer join (select solicitud_id, sum(precio_estimado * cantidad) as total from solicitud_lineas group by solicitud_id) as _b${ta} on _b${ta}.solicitud_id = ${ta}.id")
 	public BigDecimal total;
-	
+
 	@Formula(select = "_b2${ta}.totalAjuste", join = "left outer join (select solicitud_id, sum(precio*cantidad) as totalAjuste from solicitud_linea_ajustes group by solicitud_id) as _b2${ta} on _b2${ta}.solicitud_id = ${ta}.id")
 	public BigDecimal totalAjuste;
-    
+
 	public BigDecimal getTotalAjuste(){
 		if (totalAjuste == null)
 			return new BigDecimal(0);
 		return totalAjuste;
 	}
-	
+
 	public BigDecimal getTotal(){
 		if (total == null)
 			return new BigDecimal(0);
 		return total;
 	}
-	
+
 	public BigDecimal getTotalTotal(){
 		return getTotal().add(getTotalAjuste());
 	}
-	
+
 	@ManyToOne(fetch=FetchType.LAZY)
 	@JoinColumn(name="create_usuario_id", referencedColumnName="id", updatable = false, insertable = false)
-	public Usuario usuario; 
+	public Usuario usuario;
 
 	@ManyToOne
 	@JoinColumn(name="create_usuario_id", referencedColumnName="id", insertable=false, updatable=false)
-	public Usuario create_usuario; 
+	public Usuario create_usuario;
 	@Column(name="create_usuario_id")
 	public Long create_usuario_id;
-	 
-	public Date create_date; 
-	public Date write_date; 
-	
+
+	public Date create_date;
+	public Date write_date;
+
 	@ManyToOne
 	@JoinColumn(name="write_usuario_id", referencedColumnName="id", insertable=false, updatable=false)
-	public Usuario write_usuario; 
+	public Usuario write_usuario;
 	@Column(name="write_usuario_id")
 	public Long write_usuario_id;
-	
+
 	@ManyToOne
 	@JoinColumn(name="liquidacion_mes_id", referencedColumnName="id", insertable=false, updatable=false)
 	public LiquidacionMes liquidacionMes;
 	public Long liquidacion_mes_id;
-	
+
 	@ManyToOne
 	@JoinColumn(name="asignacion_usuario_id", referencedColumnName="id", insertable=false, updatable=false)
-	public Usuario asignacion_usuario; 
+	public Usuario asignacion_usuario;
 	@Column(name="asignacion_usuario_id")
 	public Long asignacion_usuario_id;
-	 
-	public Date asignacion_date; 
-	
-	
-	
+
+	public Date asignacion_date;
+
+
+
 	public static Model.Finder<Long,Solicitud> find = new Finder<Long,Solicitud>(Long.class, Solicitud.class);
-	
+
 	public Long duplicar(Long id,Long userLogin){
-		
+
 		Connection conn = null;
 		PreparedStatement stmt = null;
 		ResultSet rs = null;
 		Long ret = new Long(0);
 		try {
-			
+
 			conn = play.db.DB.getConnection();
-			
+
 			stmt = conn.prepareStatement("SELECT duplicar_solicitud(?,?)");
 			stmt.setInt(1, id.intValue());
 			stmt.setInt(2,userLogin.intValue());
 			rs = stmt.executeQuery();
-			
+
 			if (rs.next()) {
 				ret = (long) rs.getInt(1);
 			}
-			
+
 		}catch (SQLException e) {
 			Logger.error("Error duplicar: "+e);
         } finally {
@@ -208,39 +225,39 @@ public class Solicitud extends Model {
         	if (rs != null) try { rs.close(); } catch (Exception e) { }
             if (conn != null) try { conn.close(); } catch (Exception e) { }
         }
-		 
+
 		return ret;
 	}
-	
+
 	public List<Solicitud> getDataSuggest(String input,Integer limit){
 		List<Solicitud> l = find.where()
 				.ilike("referencia", "%"+input+"%")
 				.setMaxRows(limit).orderBy("referencia")
-			    .findList();  
+			    .findList();
 		return l;
 	}
 
 	public static boolean controlTildeProfe(Solicitud s){
-		
+
 		boolean ret = true;
-		
+
 		Integer sl = SolicitudLinea.find.where().in("cuenta_analitica_id", CuentaAnalitica.getCuentasAnaliticasHijas(142)).findRowCount();
-		
+
 		if(sl > 0 && (!s.tipo_cuenta_id.equals(TipoCuenta.PROFE))){
 			ret = false;
 		}
-		
+
 		return ret;
 	}
-	
-	
-	public static Pagination<Solicitud> page(String referencia, 
-											String responsable, 
-											String cliente, 
-											String expediente, 
-											String servicio, 
-											String estado, 
-											String ejercicio, 
+
+
+	public static Pagination<Solicitud> page(String referencia,
+											String responsable,
+											String cliente,
+											String expediente,
+											String servicio,
+											String estado,
+											String ejercicio,
 											String desde,
 											String hasta,
 											String producto,
@@ -262,7 +279,7 @@ public class Solicitud extends Model {
     	Pagination<Solicitud> p = new Pagination<Solicitud>();
     	p.setOrderDefault("DESC");
     	p.setSortByDefault("id");
-    	
+
     	ExpressionList<Solicitud> e = find.query().select("*, sum(id)")
 			.fetch("departamento")
 			.fetch("expediente")
@@ -271,11 +288,11 @@ public class Solicitud extends Model {
 			.fetch("cliente")
 			.fetch("cliente.obrasocial")
 			.where();
-    	
-    	
-    	
+
+
+
     	if(!Permiso.check("verTodasLasSolicitudes")){
-    		
+
     		if(Usuario.getUsurioSesion().plansumarmaterno) {
         		e = e.disjunction();
     			e = e.eq("tipo_cuenta_id",TipoCuenta.PLAN_SUMAR_MATERNO);
@@ -283,15 +300,15 @@ public class Solicitud extends Model {
     			Integer[] aa = {new Integer(1),new Integer(2),new Integer(3),new Integer(4),new Integer(5),new Integer(6),new Integer(7),new Integer(8),new Integer(9),new Integer(12),new Integer(13)};
     			e = e.not(Expr.in("expediente.ejercicio_id", aa));
     			e = e.endJunction();
-    			
-        	}	
-    		
+
+        	}
+
     		if(Usuario.getUsurioSesion().obera) {
     			Date fdesde = DateUtils.formatDate("01/08/2022", "dd/MM/yyyy");
     			e = e.ge("create_date", fdesde);
-        		 
+
         	}
-    		
+
     		Integer deptoId = 0;
     		List<Integer> l = null;
     		if( Usuario.getUsurioSesion().organigrama_id != null){
@@ -309,7 +326,7 @@ public class Solicitud extends Model {
     		    			e.eq("profe", true);
     		    		}
     		    	}else{*/
-    		    		
+
     		    		if(!tipo_cuenta_id.isEmpty()){
     		    			e = e.disjunction();
     	    				e = e.eq("tipo_cuenta_id", TipoCuenta.PROFE);
@@ -322,16 +339,16 @@ public class Solicitud extends Model {
 		    				e = e.endJunction();
     		    		}
 	    			//}
-    				
+
     			}else{
     				e = e.in("create_usuario_id", l);
     			}
     		}else{
-    			
-    			 
+
+
     				e = e.disjunction();
         			e = e.in("deposito_id",Usuario.getUsurioSesion().organigrama.deposito_id.intValue());
-        			
+
     	    			e = e.conjunction();
     	    				e = e.in("create_usuario_id",Usuario.getUsuarioSesion());
     	    				e = e.eq("deposito_id",Usuario.getUsurioSesion().organigrama.deposito_id.intValue());
@@ -339,23 +356,23 @@ public class Solicitud extends Model {
         			e = e.endJunction();
     		}
     	}
-    	
+
     	if(!obra_social_id.isEmpty()){
     		e.eq("cliente.obrasocial_id", Integer.parseInt(obra_social_id));
     	}
-    	
+
     	if(!tipo_cuenta_id.isEmpty()){
     		e.eq("tipo_cuenta_id", Integer.parseInt(tipo_cuenta_id));
     	}
-    	
+
     	if(!asignacion_usuario_id.isEmpty()){
     		e.eq("asignacion_usuario_id", Integer.parseInt(asignacion_usuario_id));
     	}
-    	
+
     	if(!nro_liquidacion_parque.isEmpty()){
     		e.eq("liquidacionMes.nro_liquidacion_parque", Integer.parseInt(nro_liquidacion_parque));
     	}
-    	
+
     	if(!desde.isEmpty()){
     		Date fdesde = DateUtils.formatDate(desde, "dd/MM/yyyy");
     		e.ge("create_date", fdesde);
@@ -365,7 +382,7 @@ public class Solicitud extends Model {
     		Date fhasta = DateUtils.formatDate(hasta, "dd/MM/yyyy");
     		e.le("create_date", fhasta);
     	}
-    	
+
     	if(!fdesdepreventiva.isEmpty()){
     		Date fdesde = DateUtils.formatDate(fdesdepreventiva, "dd/MM/yyyy");
     		e.ge("fecha_imputacion", fdesde);
@@ -375,15 +392,15 @@ public class Solicitud extends Model {
     		Date fhasta = DateUtils.formatDate(fhastapreventiva, "dd/MM/yyyy");
     		e.le("fecha_imputacion", fhasta);
     	}
-    	
+
     	if(!profe.isEmpty()){
     		if(profe.compareToIgnoreCase("SI") == 0){
     			e.eq("profe", true);
     		}else{
     			e.eq("profe", false);
     		}
-    	} 
-    	
+    	}
+
     	if(!recuperable.isEmpty()){
     		if(recuperable.compareToIgnoreCase("SI") ==0){
     			e.eq("recuperable", true);
@@ -391,7 +408,7 @@ public class Solicitud extends Model {
     			e.eq("recuperable", false);
     		}
     	}
-    	
+
     	if(!repo_stock.isEmpty()){
     		if(repo_stock.compareToIgnoreCase("SI") ==0){
     			e.eq("repo_stock", true);
@@ -399,7 +416,7 @@ public class Solicitud extends Model {
     			e.eq("repo_stock", false);
     		}
     	}
-    	
+
     	if(!entregado.isEmpty()){
     		if(entregado.compareToIgnoreCase("SI") ==0){
     			e.eq("entregado", true);
@@ -407,7 +424,7 @@ public class Solicitud extends Model {
     			e.eq("entregado", false);
     		}
     	}
-    	
+
     	if(!emergencia.isEmpty()){
     		if(emergencia.compareToIgnoreCase("SI") ==0){
     			e.eq("expediente.emergencia", true);
@@ -415,15 +432,15 @@ public class Solicitud extends Model {
     			e.eq("expediente.emergencia", false);
     		}
     	}
-    	
-    	
+
+
 
 		if(!referencia.isEmpty())
 			e.ilike("referencia", "%" + referencia + "%");
-		
+
 		if(!pedido_rismi.isEmpty())
 			e.ilike("pedido_rismi_id", "%" + pedido_rismi + "%");
-		
+
 		if(!responsable.isEmpty())
 			e.eq("create_usuario_id", Integer.parseInt(responsable));
 		if(!solicitante.isEmpty())
@@ -434,31 +451,31 @@ public class Solicitud extends Model {
         	e.eq("expediente_id", Integer.parseInt(expediente));
         if(!servicio.isEmpty())
         	e.eq("departamento_id", Integer.parseInt(servicio));
-        
+
         if(!estado.isEmpty()){
         	e.eq("estado.id", Integer.parseInt(estado));
         }
-        
+
         if( Usuario.getUsurioSesion().organigrama_id == null || (!Usuario.getUsurioSesion().organigrama_id.equals(new Long(20)) && !Usuario.getUsurioSesion().organigrama_id.equals(new Long(90)))){
     		e.ne("estado.id", Estado.SOLICITUD_ESTADO_RESERVADO);
     	}
-        
+
         if(!ejercicio.isEmpty())
         	e.eq("expediente.ejercicio_id", Integer.parseInt(ejercicio));
-        
+
         if(!producto.isEmpty())
         	e.eq("lineas.producto_id", Integer.parseInt(producto));
-        
+
         if(!cuenta_analitica_padre_id.isEmpty())
         	e.eq("lineas.cuenta_analitica_id", Integer.parseInt(cuenta_analitica_padre_id));
-        
+
     	p.setExpressionList(e);
-    	
+
     	return p;
 	}
-	
+
 	public static Integer modificarEstadoMasivo(Integer idEstado, List<Integer> solSeleccionados){
-		
+
 		SqlUpdate update = Ebean.createSqlUpdate("UPDATE solicitudes " +
 				"SET state_id = :idEstado,write_date = :write_date,write_usuario_id = :write_usuario_id " +
 				"WHERE id IN (:ids)");
@@ -466,8 +483,8 @@ public class Solicitud extends Model {
 		update.setParameter("write_date",new Date());
 		update.setParameter("write_usuario_id",new Long(Usuario.getUsuarioSesion()));
 		update.setParameter("ids", solSeleccionados);
-		
+
 		return update.execute();
 	}
-	
+
 }
