a12978cd749e2f1b7be30621cf37e254b7fcea44
Marcelo Schell <mschell@proveedor.teco.com.ar>
2025-06-12 10:29:58 -0300

    add

--- public/javascripts/recupero/planilla.js
+++ public/javascripts/recupero/planilla.js
@@ -1,8 +1,19 @@
 $( function () {
 
-	$('#searchExpediente,#searchDeposito,#searchDeposito').modalSearch();
+	$('#searchCliente,#searchPlanilla,#searchExpediente,#searchDeposito,#searchDeposito').modalSearch();
 	$("#numero").numeric_input();
-	$('#searchPlanilla,#searchCliente').modalSearch();
+
+	var options = {
+			script:"/suggestCliente/",
+			varname:"",
+			json:true,
+			shownoresults:true,
+			maxresults:6,
+			callback: function (obj) {
+										$("#cliente").next().val(obj.id);
+									 }
+		};
+	var as_json = new bsn.AutoSuggest('cliente', options);
 
 	$('#reporteDesdePlanilla').click( function() { //abrir modal para mostrar mensaje solicitud 322
 		var url = $(this).attr("data-url");
