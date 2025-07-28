75836be59666da66d5df4f4fafe5b4f2c719704f
Marcelo Schell <mschell@proveedor.teco.com.ar>
2025-07-27 21:46:09 -0300

    add

--- public/javascripts/recupero/planilla.js
+++ public/javascripts/recupero/planilla.js
@@ -42,7 +42,7 @@ $( function () {
 
 	});
 
-	$('#reporteDesdePlanillaPagos').click( function() { //abrir modal para mostrar mensaje solicitud 322
+	$('#reporteDesdePlanilla').click( function() { //abrir modal para mostrar mensaje solicitud 322
 		var url = $(this).attr("data-url");
 		var dialogo = $('<div></div>');
 		dialogo.dialog({
@@ -67,6 +67,51 @@ $( function () {
 		    }
 	      });
 
+	});
+
+	$('#accionAgregarNotaCertificadoDeuda').click( function() { //abrir modal para mostrar mensaje solicitud 322
+		var url = $(this).attr("data-url");
+		var dialogo = $('<div></div>');
+		dialogo.dialog({
+			title: "Agregar Nota",
+	    	resizable: false,
+			autoOpen: true,
+			modal: true,
+			height: 350,
+			width:750,
+	        buttons: {
+		          Cerrar: function() {
+		            $( this ).dialog( "destroy" );
+		          }
+		    },
+	    	close: function(event, ui ){
+	    		$(this).dialog( "destroy" );
+	    	},
+		    open: function( event, ui ) {
+				$.get(url,   function(data){
+					dialogo.html(data);
+				});
+		    }
+	      });
+
+	});
+
+	$(document).on("submit", '#formAgregarNotaCertificadoDeuda', function(){
+		var form = $(this);
+		var url = form.attr('action');
+		var data = form.serialize();
+
+		var submit = form.find("button[type='submit']");
+		submit.replaceWith(getLoading());
+		$.post(url, data, function(data){
+			if(data.success) {
+				location.reload();
+			} else {
+				form.replaceWith(data);
+			}
+		});
+
+		return false;
 	});
 
 
