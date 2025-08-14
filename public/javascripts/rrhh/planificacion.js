17cb607602d2310349fd4d7585d543572d188532
Marcelo Schell <mschell@proveedor.teco.com.ar>
2025-08-14 08:50:13 -0300

    adfd

---
+++ public/javascripts/rrhh/planificacion.js
@@ -0,0 +1,18 @@
+$( function(){
+
+	$('#buscarAgente,#buscarServicio,#searchAgente,#searchPeriodo').modalSearch();
+
+	$('#filtrosEstados button').click( function() {
+		var checkbox = $(this).find(':checkbox');
+		checkbox.is(':checked') ? checkbox.removeAttr('checked') : checkbox.prop('checked', true);
+
+
+		$('#filtrosEstados button:not(:has(:checkbox:checked))').removeClass('activeFiltro');
+		$('#filtrosEstados button:has(:checkbox:checked)').addClass('activeFiltro');
+
+		$(this).closest('form').submit();
+	});
+
+	$('#filtrosEstados button:has(:checkbox:checked)').addClass('activeFiltro');
+
+});
