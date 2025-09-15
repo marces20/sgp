f09e465b7953146795fdbf8838b0ed580a537787
Marcelo Schell <mschell@proveedor.teco.com.ar>
2025-09-15 19:37:42 -0300

    add

--- app/controllers/informes/InformesDeudasController.java
+++ app/controllers/informes/InformesDeudasController.java
@@ -17,6 +17,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
+import controllers.auth.CheckPermiso;
 import models.informes.HistorialDeudaProveedores;
 import models.informes.InformeDeudaPorActaMaterializada;
 import play.Logger;
@@ -29,18 +30,19 @@ import utils.RequestVar;
 import views.html.informes.informeDeuda.*;
 
 public class InformesDeudasController extends Controller {
-	
+
+	@CheckPermiso(key = "verInformeDeudas")
 	public static Result deudasFotoPorFecha() {
 		DynamicForm formBuscador = form().bindFromRequest();
-		
+
 		BigDecimal totalCorte = new BigDecimal(0);
 		BigDecimal totalHoy = new BigDecimal(0);
 		BigDecimal totalDiferencia = new BigDecimal(0);
-		
+
 		BigDecimal totalCorteE = new BigDecimal(0);
 		BigDecimal totalHoyE = new BigDecimal(0);
 		BigDecimal totalDiferenciaE = new BigDecimal(0);
-		
+
 		BigDecimal totalCorteO = new BigDecimal(0);
 		BigDecimal totalHoyO = new BigDecimal(0);
 		BigDecimal totalDiferenciaO = new BigDecimal(0);
@@ -48,74 +50,74 @@ public class InformesDeudasController extends Controller {
 		String fechaHoyString= null;
 		if(!RequestVar.get("fecha_corte").isEmpty()){
 			fString = RequestVar.get("fecha_corte");
-			
+
 			Date fechaHoy = new Date();
 			SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
 			fechaHoyString = simpleDateFormat.format(fechaHoy);
-			
-			Date fecha = DateUtils.formatDate(RequestVar.get("fecha_corte"), "dd/MM/yyyy"); 
-			
+
+			Date fecha = DateUtils.formatDate(RequestVar.get("fecha_corte"), "dd/MM/yyyy");
+
 			List<SqlRow> requipo = HistorialDeudaProveedores.getTotalesPorCorteFechaNuevo(true,fecha,true,false);
 			for(SqlRow e : requipo) {
 				totalCorte = totalCorte.add(e.getBigDecimal("total_deuda"));
 				totalCorteE = totalCorteE.add(e.getBigDecimal("total_deuda"));
 			}
-			
+
 			List<SqlRow> rtotros = HistorialDeudaProveedores.getTotalesPorCorteFechaNuevo(true,fecha,false,false);
 			for(SqlRow o : rtotros) {
 				totalCorte = totalCorte.add(o.getBigDecimal("total_deuda"));
 				totalCorteO = totalCorteO.add(o.getBigDecimal("total_deuda"));
 			}
-			
+
 			List<SqlRow> requipoHoy = HistorialDeudaProveedores.getTotalesPorCorteFechaNuevo(true,fecha,true,true);
 			for(SqlRow e : requipoHoy) {
 				totalHoy = totalHoy.add(e.getBigDecimal("total_deuda"));
 				totalHoyE = totalHoyE.add(e.getBigDecimal("total_deuda"));
 			}
-			
+
 			List<SqlRow> rtotrosHoy = HistorialDeudaProveedores.getTotalesPorCorteFechaNuevo(true,fecha,false,true);
 			for(SqlRow o : rtotrosHoy) {
 				totalHoy = totalHoy.add(o.getBigDecimal("total_deuda"));
 				totalHoyO = totalHoyO.add(o.getBigDecimal("total_deuda"));
 			}
-			
-			 
-			
-			
-			
-			
-			
+
+
+
+
+
+
+
 			totalDiferencia = totalCorte.subtract(totalHoy);
 			totalDiferenciaE = totalCorteE.subtract(totalHoyE);
 			totalDiferenciaO = totalCorteO.subtract(totalHoyO);
-			
-			
-			
-			
-			
+
+
+
+
+
 		}else {
 			flash("error", "Debe Seleccionar una Fecha de Corte.");
 		}
-		
-		
+
+
 		return ok(deudaFotoPorFecha.render(formBuscador,fString,fechaHoyString,
 				totalCorte,totalHoy,totalDiferencia,
 				totalCorteE,totalHoyE,totalDiferenciaE,
 				totalCorteO,totalHoyO,totalDiferenciaO));
 	}
-	
-	
+
+
 	/*public static Result deudasFotoPorFechaOld() {
 		DynamicForm formBuscador = form().bindFromRequest();
-		
+
 		BigDecimal totalCorte = new BigDecimal(0);
 		BigDecimal totalHoy = new BigDecimal(0);
 		BigDecimal totalDiferencia = new BigDecimal(0);
-		
+
 		BigDecimal totalCorteE = new BigDecimal(0);
 		BigDecimal totalHoyE = new BigDecimal(0);
 		BigDecimal totalDiferenciaE = new BigDecimal(0);
-		
+
 		BigDecimal totalCorteO = new BigDecimal(0);
 		BigDecimal totalHoyO = new BigDecimal(0);
 		BigDecimal totalDiferenciaO = new BigDecimal(0);
@@ -123,36 +125,36 @@ public class InformesDeudasController extends Controller {
 		String fechaHoyString= null;
 		if(!RequestVar.get("fecha_corte").isEmpty()){
 			fString = RequestVar.get("fecha_corte");
-			
+
 			Date fechaHoy = new Date();
 			SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
 			fechaHoyString = simpleDateFormat.format(fechaHoy);
-			
-			Date fecha = DateUtils.formatDate(RequestVar.get("fecha_corte"), "dd/MM/yyyy"); 
-			
+
+			Date fecha = DateUtils.formatDate(RequestVar.get("fecha_corte"), "dd/MM/yyyy");
+
 			List<SqlRow> requipo = HistorialDeudaProveedores.getTotalesPorCorteFecha(true,fecha,true);
 			for(SqlRow e : requipo) {
-				
+
 				totalCorte = totalCorte.add(e.getBigDecimal("total_deuda"));
 				totalHoy = totalHoy.add(e.getBigDecimal("total"));
-				
+
 				totalCorteE = totalCorteE.add(e.getBigDecimal("total_deuda"));
 				totalHoyE = totalHoyE.add(e.getBigDecimal("total"));
-				
-				
+
+
 				//e.getBigDecimal("total_deuda_hoy");
-				
+
 			}
-			
+
 			List<SqlRow> rtotros = HistorialDeudaProveedores.getTotalesPorCorteFecha(true,fecha,false);
 			for(SqlRow o : rtotros) {
 				totalCorte = totalCorte.add(o.getBigDecimal("total_deuda"));
 				totalHoy = totalHoy.add(o.getBigDecimal("total"));
-				
+
 				totalCorteO = totalCorteO.add(o.getBigDecimal("total_deuda"));
-				
+
 				totalHoyO = totalHoyO.add(o.getBigDecimal("total"));
-				
+
 			}
 			totalDiferencia = totalCorte.subtract(totalHoy);
 			totalDiferenciaE = totalCorteE.subtract(totalHoyE);
@@ -160,27 +162,29 @@ public class InformesDeudasController extends Controller {
 		}else {
 			flash("error", "Debe Seleccionar una Fecha de Corte.");
 		}
-		
-		
+
+
 		return ok(deudaFotoPorFecha.render(formBuscador,fString,fechaHoyString,totalCorte,totalHoy,totalDiferencia
 				,totalCorteE,totalHoyE,totalDiferenciaE
 				,totalCorteO,totalHoyO,totalDiferenciaO));
 	}	*/
-	
+
+	@CheckPermiso(key = "verInformeDeudas")
 	public static Result deudasGenerales() {
-		
-		
-		
-		 
-		
-		
+
+
+
+
+
+
 		DynamicForm formBuscador = form().bindFromRequest();
 		return ok(deudaGeneral.render(formBuscador));
-	}	
-	
+	}
+
+	@CheckPermiso(key = "verInformeDeudas")
 	public static Result getDatosInformesDeudaAntiguedadInstitucion(){
 		ObjectNode rpta = Json.newObject();
-		ArrayNode results = rpta.arrayNode(); 
+		ArrayNode results = rpta.arrayNode();
 		InformeDeudaPorActaMaterializada.actualizarVistaMaterializada();
 		List<SqlRow> s = InformeDeudaPorActaMaterializada.getDeudaPorInstitucionPorAntiguedadPorEjercicioResumen(null);
 		List<String> header = new ArrayList<String>();
@@ -190,25 +194,25 @@ public class InformesDeudasController extends Controller {
 			if(!header.contains(sx.getString("deposito"))) {
 					header.add(sx.getString("deposito"));
 					restHeaderJs.put(sx.getString("deposito"), sx.getString("deposito"));
-			}		
+			}
 		}
-		
+
 		for(SqlRow sx : s) {
-			
-			
+
+
 			//String[] fecha_mes_ano_array = sx.getString("fecha_mes_ano").split("/");
 			//String fecha_mes_ano = fecha_mes_ano_array[1]+fecha_mes_ano_array[0];
 			String fecha_mes_ano = sx.getString("fecha_ano");
-					
+
 			if(dat.containsKey(fecha_mes_ano)) {
-				
+
 				Map<String,BigDecimal> tmp = dat.get(fecha_mes_ano);
-				
+
 				tmp.put(sx.getString("deposito"), sx.getBigDecimal("total_deuda"));
-				
+
 				dat.put(fecha_mes_ano, tmp);
 			}else {
-				
+
 				Map<String,BigDecimal> i = new HashMap<String,BigDecimal>();
 				for(String insti : header) {
 					i.put(insti, BigDecimal.ZERO);
@@ -218,17 +222,17 @@ public class InformesDeudasController extends Controller {
 				dat.put(fecha_mes_ano, treeMapi);
 			}
 		}
-		
+
 		Map<String,Map<String,BigDecimal>> treeMap = new TreeMap<String,Map<String,BigDecimal>>(dat);
-		ObjectNode restJs = (ObjectNode) Json.toJson(treeMap); 
-		Logger.debug("11111111111 "+restJs); 
-		Logger.debug("22222222222 "+restHeaderJs); 
-		
+		ObjectNode restJs = (ObjectNode) Json.toJson(treeMap);
+		Logger.debug("11111111111 "+restJs);
+		Logger.debug("22222222222 "+restHeaderJs);
+
 		rpta.put("header", restHeaderJs);
 		rpta.put("datos", restJs);
 		rpta.put("success", true);
-		 
-		
+
+
 		return ok(rpta);
 	}
 }
