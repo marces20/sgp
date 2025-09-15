7ff9e6d107f82e46f53ea4f8e5acb4a56a31806f
Marcelo Schell <mschell@proveedor.teco.com.ar>
2025-09-15 19:36:53 -0300

    add

---
+++ app/controllers/informes/InformeLaboratorioController.java
@@ -0,0 +1,155 @@
+package controllers.informes;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import com.avaje.ebean.Ebean;
+import com.avaje.ebean.SqlQuery;
+import com.avaje.ebean.SqlRow;
+
+import controllers.Secured;
+import models.OrdenRubro;
+import models.Periodo;
+import play.mvc.Controller;
+import play.mvc.Result;
+import play.mvc.Security;
+import views.html.informes.informeLaboratorio.*;
+
+@Security.Authenticated(Secured.class)
+public class InformeLaboratorioController extends Controller {
+
+	public static Result controlProductoPorMesPorInstitucion() {
+
+		Periodo px = Periodo.getPeriodoByDate(new Date());
+		int periodo6meses = px.id.intValue() -12 ;
+		List<Periodo> p = Periodo.find.where().ge("id", periodo6meses).ge("id", 163).le("id", px.id).orderBy("id asc").findList();
+
+
+		String sql  = "SELECT o.orden_linea_id as orden_linea_id, cantidad, precio, id, nombre,deposito, recepcionado, udm, pendiente, " +
+				"	 o.totalRecepcionado totalRecepcionado, anulados,mes FROM ( " +
+				"	  SELECT ol.id as orden_linea_id, " +
+				"	 	 CASE WHEN ola.cantidad IS NOT NULL THEN (ol.cantidad - ola.cantidad) " +
+				"	 		  ELSE ol.cantidad  " +
+				"	 		  END as cantidad, " +
+				"	 	  (ol.precio) as precio, p.id,p.nombre,  " +
+				"	 	  coalesce(SUM(linea.cantidad),0) recepcionado, " +
+				"	 	  u.nombre as udm, " +
+				"	 	  ((ol.cantidad - coalesce(SUM(linea.cantidad),0)) - coalesce(SUM(ola.cantidad),0)) as pendiente,  " +
+				"	 	  coalesce(sum(linea.precio),0) as totalRecepcionado, " +
+				"	  	  coalesce(sum(ola.cantidad),0) as anulados," +
+				"		  linea.mes as mes, "+
+				"		  de.nombre as deposito" +
+				"		 " +
+				"	  from orden_lineas ol " +
+				"		 " +
+				"	  left join " +
+				"	  (   " +
+				"		  select rec.orden_provision_id, SUM(rl.cantidad) cantidad, rl.linea_orden_id, ol.precio,to_char(rem.fecha_remito,'MM/yyyy') as mes" +
+				"		  from recepciones rec " +
+				"		  inner join remitos rem on rec.id = rem.recepcion_id " +
+				"		  inner join remitos_lineas rl on rem.id = rl.remito_id " +
+				"		  inner join orden_lineas ol on ol.id = rl.linea_orden_id " +
+				"	   	  GROUP BY rec.orden_provision_id,rl.linea_orden_id, ol.precio,to_char(rem.fecha_remito,'MM/yyyy')" +
+				"	   " +
+				"	  ) as linea on ol.id = linea.linea_orden_id " +
+				"	  left join ordenes o on o.id = ol.orden_id  " +
+				"	  left join productos p on p.id = ol.producto_id " +
+				"	  left join depositos de on de.id = o.deposito_id  " +
+				"	  left join udms u on u.id = ol.udm_id " +
+				"	  left join (select orden_linea_id, SUM(cantidad) cantidad from orden_lineas_anulaciones GROUP BY orden_linea_id) ola on ola.orden_linea_id = ol.id " +
+				"	 " +
+				" 	  WHERE o.orden_subrubro_id = 614 AND linea.mes is not null AND o.deposito_id in(1,3,32)" +
+				"	  group by ol.id, p.id, p.nombre, u.nombre, ola.cantidad,linea.mes,de.nombre  ORDER BY p.nombre ASC " +
+				"	 ) as o";
+
+		SqlQuery sqlQuery = Ebean.createSqlQuery(sql);
+
+
+		List<SqlRow>  row = sqlQuery.findList();
+
+		Map<String,Integer> periodoTotal = new HashMap<>();
+		Map<String,Map<String,Integer>> productoPeriodoTotal = new HashMap<>();
+		Map<String,Map<String,Map<String,Integer>>> OrganigramaRubroPeriodoTotal = new HashMap<>();
+
+		for(SqlRow sr : row) {
+
+			Map<String,Integer> periodoTotalTmp =new HashMap<>();
+			Map<String,Map<String,Integer>> rubroPeriodoTotalTmp = new HashMap<>();
+			Map<String,Map<String,Map<String,Integer>>> OrganigramaRubroPeriodoTotalTmp = new HashMap<>();
+
+			String depo = sr.getString("deposito");
+			String producto = sr.getString("nombre");
+			String periodo = sr.getString("mes");
+			Integer cantidad = sr.getInteger("recepcionado");
+
+			if(OrganigramaRubroPeriodoTotal.containsKey(depo)) {// si esta el organigrama
+
+				rubroPeriodoTotalTmp = OrganigramaRubroPeriodoTotal.get(depo);// traigo el organigrama
+
+				if(OrganigramaRubroPeriodoTotal.get(depo).containsKey(producto)) {// si tiene el rubro el organigrama
+
+					OrganigramaRubroPeriodoTotalTmp = OrganigramaRubroPeriodoTotal;
+
+					periodoTotalTmp = rubroPeriodoTotalTmp.get(producto);
+					periodoTotalTmp.put(periodo, cantidad);
+
+					rubroPeriodoTotalTmp.put(producto,periodoTotalTmp);
+
+
+
+					OrganigramaRubroPeriodoTotalTmp.put(depo,rubroPeriodoTotalTmp);
+
+					OrganigramaRubroPeriodoTotal = OrganigramaRubroPeriodoTotalTmp;
+
+
+				}else {
+
+					periodoTotalTmp.put(periodo, cantidad);
+
+					OrganigramaRubroPeriodoTotalTmp = OrganigramaRubroPeriodoTotal;
+
+					rubroPeriodoTotalTmp = OrganigramaRubroPeriodoTotal.get(depo);
+					rubroPeriodoTotalTmp.put(producto,periodoTotalTmp);
+
+					OrganigramaRubroPeriodoTotalTmp.put(depo,rubroPeriodoTotalTmp);
+
+					OrganigramaRubroPeriodoTotal = OrganigramaRubroPeriodoTotalTmp;
+
+
+				}
+
+			}else {
+
+				periodoTotal = new HashMap<>();
+				periodoTotal.put(periodo, cantidad);
+				productoPeriodoTotal = new HashMap<>();
+				productoPeriodoTotal.put(producto, periodoTotal);
+				OrganigramaRubroPeriodoTotal.put(depo, productoPeriodoTotal);
+			}
+		}
+
+		Map<String,Map<String,Map<String,Integer>>> OrganigramaRubroPeriodoTotalTmp = new HashMap<>();
+		for(Map.Entry<String,Map<String,Map<String,Integer>>> rubroPeriodoTotalTmp : OrganigramaRubroPeriodoTotal.entrySet()) {
+
+			Map<String,Map<String,Integer>> rubroPeriodoTotalTree = new TreeMap(rubroPeriodoTotalTmp.getValue());
+
+			OrganigramaRubroPeriodoTotalTmp.put(rubroPeriodoTotalTmp.getKey(), rubroPeriodoTotalTree);
+		}
+
+		Map<String,Map<String,Map<String,Integer>>> OrganigramaRubroPeriodoTotalTree = new TreeMap(OrganigramaRubroPeriodoTotalTmp);
+
+		//Map<String,String> ordenRubro = new HashMap<>();
+
+		//for(OrdenRubro orx:OrdenRubro.find.orderBy("orden asc").findList()) {
+		//ordenRubro.put(orx.orden, orx.nombre);
+		//}
+
+		return ok(controlProductoPorMesPorInstitucion.render(OrganigramaRubroPeriodoTotalTree,p));
+
+	}
+}
+
--- app/controllers/informes/InformesHonorariosController.java
+++ app/controllers/informes/InformesHonorariosController.java
@@ -10,6 +10,7 @@ import com.avaje.ebean.SqlRow;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
+import controllers.auth.CheckPermiso;
 import models.Periodo;
 import models.haberes.LiquidacionMes;
 import models.haberes.LiquidacionTipo;
@@ -22,20 +23,21 @@ import utils.RequestVar;
 import views.html.informes.informeHonorario.*;
 
 public class InformesHonorariosController extends Controller {
-	
+
+	@CheckPermiso(key = "verInformesHonorarios")
 	public static Result honorarioRrhhPorPeriodo() {
 		Long pId = null;
 		Integer total= new Integer(0);
 		Integer totalconve = new Integer(0);
 		Integer totalparque = new Integer(0);
-		
+
 		if(!RequestVar.get("periodo.id").isEmpty()){
 			pId = new Long(RequestVar.get("periodo.id"));
-			
+
 			List<SqlRow> l = LiquidacionMes.getCountRelacionPorPeriodoNew(pId);
-					
+
 			for(SqlRow lx : l) {
-				
+
 				if(lx.getBoolean("cm")) {
 					totalconve = lx.getInteger("count");
 					total = total+ totalconve;
@@ -44,43 +46,44 @@ public class InformesHonorariosController extends Controller {
 					total = total+ totalparque;
 				}
 			}
-			
-			
+
+
 		}else {
 			flash("error", "Debe Seleccionar un Periodo.");
 		}
-		
+
 		DynamicForm formBuscador = form().bindFromRequest();
 		return ok(honorarioRrhhPorPeriodo.render(formBuscador,pId,total,totalconve,totalparque));
 	}
-	
+
+	@CheckPermiso(key = "verInformesHonorarios")
 	public static Result getCountRrrhPorEscala(){
 		ObjectNode rpta = Json.newObject();
 	    ArrayNode results = rpta.arrayNode();
 	    ObjectNode response = Json.newObject();
-	    
+
 	    Long idPeriodo = null;
 	    Boolean convenio = null;
-	    
+
 	    if(request().body().asFormUrlEncoded().get("idPeriodo")[0] != null && !request().body().asFormUrlEncoded().get("idPeriodo")[0].isEmpty()) {
 	    	idPeriodo = new Long(request().body().asFormUrlEncoded().get("idPeriodo")[0]);
 	    }
-	    
+
 	    if(request().body().asFormUrlEncoded().get("convenio")[0] != null && !request().body().asFormUrlEncoded().get("convenio")[0].isEmpty()) {
 	    	convenio = new Boolean(request().body().asFormUrlEncoded().get("convenio")[0]);
 	    }
-	     
-	    
+
+
 	    if(idPeriodo != null && convenio != null){
-	    
+
 		    try {
 				List<SqlRow> res = LiquidacionMes.getCountPorEscalaNew(idPeriodo, convenio);
-				
+
 				ObjectNode restJs = Json.newObject();
 				for (SqlRow sr : res) {
 					restJs.put(sr.getLong("escala_id").toString(),sr.getInteger("count"));
 				}
-				
+
 				results.add(restJs);
 				response.put("success", true);
 				response.put("results", results);
@@ -88,70 +91,70 @@ public class InformesHonorariosController extends Controller {
 		    	response.put("success", false);
 			}
 	    }
-		
+
 		return ok(response);
 	}
-	
-	
+
+	@CheckPermiso(key = "verInformesHonorarios")
 	public static Result honorario() {
-		
-		
+
+
 		BigDecimal total= new BigDecimal(0);
 		BigDecimal totalsp = new BigDecimal(0);
 		BigDecimal totalp = new BigDecimal(0);
-		
+
 		BigDecimal totalParque= new BigDecimal(0);
 		BigDecimal totalspParque = new BigDecimal(0);
 		BigDecimal totalpParque = new BigDecimal(0);
-		
+
 		BigDecimal totalConvenio= new BigDecimal(0);
 		BigDecimal totalspConvenio = new BigDecimal(0);
 		BigDecimal totalpConvenio = new BigDecimal(0);
-		
+
 		BigDecimal contribucionesPatronalesObraSocial = new BigDecimal(0);
 		BigDecimal contribucionesPatronalesJubilacion = new BigDecimal(0);
-		
-		BigDecimal porcentaje_patronal_ob = new BigDecimal(0); 
-		BigDecimal porcentaje_patronal_jub = new BigDecimal(0); 
+
+		BigDecimal porcentaje_patronal_ob = new BigDecimal(0);
+		BigDecimal porcentaje_patronal_jub = new BigDecimal(0);
 		Long pId = null;
 		if(!RequestVar.get("periodo.id").isEmpty()){
-			
+
 			pId = new Long(RequestVar.get("periodo.id"));
-			
+
 			porcentaje_patronal_ob = Periodo.find.byId(pId).getPatronalObraSocialPorcentaje().divide(new BigDecimal(100));
 			porcentaje_patronal_jub = Periodo.find.byId(pId).getPatronalJubilacionPorcentaje().divide(new BigDecimal(100));
-			
-			
+
+
 			List<SqlRow> l = LiquidacionMes.getTotalesPorTipoConcepto(pId);
-			
+
 			for(SqlRow lx : l) {
 				Logger.debug(lx.toString());
 				Logger.debug(lx.getBigDecimal("total_ca").toString());
-				
+
 				total = total.add(lx.getBigDecimal("total_ca")).add(lx.getBigDecimal("total_sa")).add(lx.getBigDecimal("total_retencion"));
-				
-				
+
+
 				//(liquidacion_tipo_id.compareTo((int)LiquidacionTipo.AYUDA_ESCOLAR) != 0 && liquidacion_tipo_id.compareTo((int)LiquidacionTipo.SAC) != 0)
-				
+
 				if( (lx.getLong("liquidacion_tipo_id").compareTo(LiquidacionTipo.AYUDA_ESCOLAR) != 0) && (lx.getLong("liquidacion_tipo_id").compareTo(LiquidacionTipo.SAC) != 0) ){
 					contribucionesPatronalesObraSocial = contribucionesPatronalesObraSocial.add(lx.getBigDecimal("total_ca").multiply(porcentaje_patronal_ob).setScale(2, BigDecimal.ROUND_HALF_UP));
 				}
 				if( (lx.getLong("liquidacion_tipo_id").compareTo(LiquidacionTipo.AYUDA_ESCOLAR) != 0)){
 					contribucionesPatronalesJubilacion = contribucionesPatronalesJubilacion.add(lx.getBigDecimal("total_ca").multiply(porcentaje_patronal_jub).setScale(2, BigDecimal.ROUND_HALF_UP));
 				}
-				
+
 				totalp = totalp.add(contribucionesPatronalesObraSocial).add(contribucionesPatronalesJubilacion);
-				
-				
+
+
 				if(lx.getBoolean("convenio_ministerio")) {
 					totalConvenio = totalConvenio.add(lx.getBigDecimal("total_ca")).add(lx.getBigDecimal("total_sa")).add(lx.getBigDecimal("total_retencion"));
 					totalpConvenio = totalpConvenio.add(contribucionesPatronalesObraSocial).add(contribucionesPatronalesJubilacion);
-					
+
 				}else {
 					totalParque = totalParque.add(lx.getBigDecimal("total_ca")).add(lx.getBigDecimal("total_sa")).add(lx.getBigDecimal("total_retencion"));
 					totalpParque = totalpParque.add(contribucionesPatronalesObraSocial).add(contribucionesPatronalesJubilacion);
 				}
-				
+
 			}
 			totalspConvenio = totalConvenio.subtract(totalpConvenio);
 			totalspParque = totalParque.subtract(totalpParque);
@@ -159,14 +162,14 @@ public class InformesHonorariosController extends Controller {
 		}else {
 			flash("error", "Debe Seleccionar un Periodo.");
 		}
-		
-		
-		
-		
-		
+
+
+
+
+
 		//LiquidacionMes.getCostoTotalPorTipoConcepto(new Long(119), false);
-		
-		
+
+
 		DynamicForm formBuscador = form().bindFromRequest();
 		return ok(honorario.render(formBuscador,pId,total,
 												totalp,
@@ -174,34 +177,35 @@ public class InformesHonorariosController extends Controller {
 												totalParque,totalpParque,totalspParque,
 												totalConvenio,totalpConvenio,totalspConvenio));
 	}
-	
+
+	@CheckPermiso(key = "verInformesHonorarios")
 	public static Result getCostoPorEscala(){
 		ObjectNode rpta = Json.newObject();
 	    ArrayNode results = rpta.arrayNode();
 	    ObjectNode response = Json.newObject();
-	    
+
 	    Long idPeriodo = null;
 	    Boolean convenio = null;
-	    
+
 	    if(request().body().asFormUrlEncoded().get("idPeriodo")[0] != null && !request().body().asFormUrlEncoded().get("idPeriodo")[0].isEmpty()) {
 	    	idPeriodo = new Long(request().body().asFormUrlEncoded().get("idPeriodo")[0]);
 	    }
-	    
+
 	    if(request().body().asFormUrlEncoded().get("convenio")[0] != null && !request().body().asFormUrlEncoded().get("convenio")[0].isEmpty()) {
 	    	convenio = new Boolean(request().body().asFormUrlEncoded().get("convenio")[0]);
 	    }
-	     
-	    
+
+
 	    if(idPeriodo != null && convenio != null){
-	    
+
 		    try {
 				List<SqlRow> res = LiquidacionMes.getCostoTotalPorEscalaNew(idPeriodo, convenio);
-				
+
 				ObjectNode restJs = Json.newObject();
 				for (SqlRow sr : res) {
 					restJs.put(sr.getLong("escala_id").toString(),sr.getDouble("monto"));
 				}
-				
+
 				results.add(restJs);
 				response.put("success", true);
 				response.put("results", results);
@@ -209,37 +213,38 @@ public class InformesHonorariosController extends Controller {
 		    	response.put("success", false);
 			}
 	    }
-		
+
 		return ok(response);
 	}
-	
+
+	@CheckPermiso(key = "verInformesHonorarios")
 	public static Result getCostoPorTipoConcepto(){
 		ObjectNode rpta = Json.newObject();
 	    ArrayNode results = rpta.arrayNode();
 	    ObjectNode response = Json.newObject();
-	    
+
 	    Long idPeriodo = null;
 	    Boolean convenio = null;
-	    
+
 	    if(request().body().asFormUrlEncoded().get("idPeriodo")[0] != null && !request().body().asFormUrlEncoded().get("idPeriodo")[0].isEmpty()) {
 	    	idPeriodo = new Long(request().body().asFormUrlEncoded().get("idPeriodo")[0]);
 	    }
-	    
+
 	    if(request().body().asFormUrlEncoded().get("convenio")[0] != null && !request().body().asFormUrlEncoded().get("convenio")[0].isEmpty()) {
 	    	convenio = new Boolean(request().body().asFormUrlEncoded().get("convenio")[0]);
 	    }
-	     
-	    
+
+
 	    if(idPeriodo != null && convenio != null){
-	    
+
 		    try {
 				List<SqlRow> res = LiquidacionMes.getCostoTotalPorTipoConceptoNew(idPeriodo, convenio);
-				
+
 				ObjectNode restJs = Json.newObject();
 				for (SqlRow sr : res) {
 					restJs.put(sr.getLong("tipo").toString(),sr.getDouble("monto"));
 				}
-				
+
 				results.add(restJs);
 				response.put("success", true);
 				response.put("results", results);
@@ -248,37 +253,37 @@ public class InformesHonorariosController extends Controller {
 		    	Logger.debug(e.toString());
 			}
 	    }
-		
+
 		return ok(response);
 	}
-	
+
 	public static Result getCostoPorClasificacionConcepto(){
 		ObjectNode rpta = Json.newObject();
 	    ArrayNode results = rpta.arrayNode();
 	    ObjectNode response = Json.newObject();
-	    
+
 	    Long idPeriodo = null;
 	    Boolean convenio = null;
-	    
+
 	    if(request().body().asFormUrlEncoded().get("idPeriodo")[0] != null && !request().body().asFormUrlEncoded().get("idPeriodo")[0].isEmpty()) {
 	    	idPeriodo = new Long(request().body().asFormUrlEncoded().get("idPeriodo")[0]);
 	    }
-	    
+
 	    if(request().body().asFormUrlEncoded().get("convenio")[0] != null && !request().body().asFormUrlEncoded().get("convenio")[0].isEmpty()) {
 	    	convenio = new Boolean(request().body().asFormUrlEncoded().get("convenio")[0]);
 	    }
-	     
-	    
+
+
 	    if(idPeriodo != null && convenio != null){
-	    
+
 		    try {
 				List<SqlRow> res = LiquidacionMes.getCostoTotalPorClasificacionConceptoNew(idPeriodo, convenio);
-				
+
 				ObjectNode restJs = Json.newObject();
 				for (SqlRow sr : res) {
 					restJs.put(sr.getLong("clasificacion_id").toString(),sr.getDouble("monto"));
 				}
-				
+
 				results.add(restJs);
 				response.put("success", true);
 				response.put("results", results);
@@ -287,9 +292,9 @@ public class InformesHonorariosController extends Controller {
 		    	Logger.debug(e.toString());
 			}
 	    }
-		
+
 		return ok(response);
 	}
-	
-	
+
+
 }
