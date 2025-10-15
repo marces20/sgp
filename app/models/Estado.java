cfba662c20c9db9500b0f45ce1d55a50eaf678a2
Marcelo Schell <mschell@proveedor.teco.com.ar>
2025-10-15 16:23:37 -0300

    add

--- app/models/Estado.java
+++ app/models/Estado.java
@@ -223,6 +223,7 @@ private static final long serialVersionUID = 1L;
 	public static final int PLANIFICIACION_ENCURSO= 143;
 	public static final int PLANIFICIACION_APROBADO_RRHH = 144;
 	public static final int PLANIFICIACION_APROBADO_LIQUIDACIONES = 145;
+	public static final int PLANIFICIACION_APROBADO_ASISTENCIAL = 146;
 	//ULTIMOOOO ID ES 124
 
 	@Id
