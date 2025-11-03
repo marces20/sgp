a67d5ca20d58c40518d659f757be349784e09f6e
Marcelo Schell <mschell@proveedor.teco.com.ar>
2025-11-02 22:03:33 -0300

    add

--- app/controllers/contabilidad/EjerciciosController.java
+++ app/controllers/contabilidad/EjerciciosController.java
@@ -32,13 +32,13 @@ import views.html.contabilidad.ejercicios.*;
 
 @Security.Authenticated(Secured.class)
 public class EjerciciosController extends Controller {
-	
+
 	final static Form<Ejercicio> ejercicioForm = form(Ejercicio.class);
-	
+
 	public static Result URL_LISTA_EJERCICIO = redirect(
 			controllers.contabilidad.routes.EjerciciosController.indexEjercicio()
 	);
-	
+
 	@CheckPermiso(key = "ejercicioGeneral")
 	public static Result indexEjercicio() {
 		DynamicForm d = form().bindFromRequest();
@@ -50,18 +50,18 @@ public class EjerciciosController extends Controller {
 								 ),
 								 d));
 	}
-	
+
 	@CheckPermiso(key = "ejercicioGeneral")
 	public static Result crearEjercicio() {
 		Form<Ejercicio> ejercicioForm = form(Ejercicio.class);
 		return ok(crearEjercicio.render(ejercicioForm));
 	}
-	
+
 	@CheckPermiso(key = "ejercicioGeneral")
 	public static Result guardarEjercicio() {
-		
+
 		Form<Ejercicio> ejercicioForm = form(Ejercicio.class).bindFromRequest();
-		
+
 		try {
 			if(ejercicioForm.hasErrors()) {
 				flash("error", "Error en formulario");
@@ -71,7 +71,7 @@ public class EjerciciosController extends Controller {
 				e.create_usuario_id = new Long(Usuario.getUsuarioSesion());
 				e.create_date = new Date();
 				e.save();
-				
+
 				flash("success", "El ejercicio se ha actualizado");
 			}
 		} catch (PersistenceException pe){
@@ -79,24 +79,24 @@ public class EjerciciosController extends Controller {
 			flash("error", "No se ha podido almacenar el ejercicio");
 			return badRequest(crearEjercicio.render(ejercicioForm));
 		}
-		
+
 		return URL_LISTA_EJERCICIO;
 	}
-	
+
 	@CheckPermiso(key = "ejercicioGeneral")
 	public static Result editarEjercicio(Long id) {
 		Ejercicio ejercicio = Ebean.find(Ejercicio.class, id);
-		
+
 		return ok(editarEjercicio.render(ejercicioForm.fill(ejercicio)));
 	}
-	
+
 	@CheckPermiso(key = "ejercicioGeneral")
 	public static Result actualizarEjercicio(){
-		
+
 		Form<Ejercicio> ejercicioForm = form(Ejercicio.class).bindFromRequest();
-		
+
 		try {
-			
+
 			if(ejercicioForm.hasErrors()) {
 				flash("error", "Error en formulario");
 				return badRequest(editarEjercicio.render(ejercicioForm));
@@ -105,10 +105,10 @@ public class EjerciciosController extends Controller {
 				e.write_usuario_id = new Long(Usuario.getUsuarioSesion());
 				e.write_date = new Date();
 				e.update();
-				
+
 				flash("success", "El ejercicio se ha actualizado");
 			}
-			
+
 		} catch (PersistenceException pe){
 			play.Logger.error("excepcion", pe);
 			flash("error", "No se ha podido almacenar el ejercicio");
@@ -116,7 +116,7 @@ public class EjerciciosController extends Controller {
 		}
 		return URL_LISTA_EJERCICIO;
 	}
-	
+
 	@CheckPermiso(key = "ejercicioGeneral")
 	public static Result eliminarEjercicio(Long id) {
 		try {
@@ -129,14 +129,14 @@ public class EjerciciosController extends Controller {
 
 		return URL_LISTA_EJERCICIO;
 	}
-	
+
 	public static Result suggestEjercicio(String input) {
-		 
+
 		ObjectNode rpta = Json.newObject();
 	    ArrayNode ejercicio = rpta.arrayNode();
-	    
+
 	    Ejercicio ad = new Ejercicio();
-		 
+
 		for(Ejercicio a : ad.getDataSuggest(input, 25)){
 			ObjectNode restJs = Json.newObject();
 	        restJs.put("id", a.id);
@@ -144,21 +144,21 @@ public class EjerciciosController extends Controller {
 	        restJs.put("info", "");
 	        ejercicio.add(restJs);
 		}
-		
+
 		ObjectNode response = Json.newObject();
 		response.put("results", ejercicio);
-		 
+
 		return ok(response);
 	}
-	
-	
+
+
 	public static Result get(int id){
-		Ejercicio ejercicio = Ejercicio.find.select("id, nombre").where().eq("id", id).findUnique();
-		
+		Ejercicio ejercicio = Ejercicio.find.select("id, nombre").where().eq("activo",true).eq("id", id).findUnique();
+
 		ObjectNode obj = Json.newObject();
 	    ArrayNode nodo = obj.arrayNode();
 		ObjectNode restJs = Json.newObject();
-		
+
 		if(ejercicio == null) {
 			restJs.put("success", false);
 			restJs.put("message", "No se encuentra el ejercicio");
@@ -170,12 +170,12 @@ public class EjerciciosController extends Controller {
 		nodo.add(restJs);
 		return ok(restJs);
 	}
-	
+
 	public static Result modalBuscar() {
     	Pagination<Ejercicio> p = new Pagination<Ejercicio>();
     	p.setOrderDefault("DESC");
-    	p.setSortByDefault("id");	
-    	p.setExpressionList(Ejercicio.find.where().ilike("nombre", "%" + RequestVar.get("nombre") + "%"));
+    	p.setSortByDefault("id");
+    	p.setExpressionList(Ejercicio.find.where().eq("activo",true).ilike("nombre", "%" + RequestVar.get("nombre") + "%"));
 		return ok( modalBusquedaEjercicio.render(p, form().bindFromRequest()) );
 	}
 }
