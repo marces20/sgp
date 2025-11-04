68c19639c5ef53caa92eef997ec0f40ffd8fe038
Marcelo Schell <mschell@proveedor.teco.com.ar>
2025-11-03 22:20:30 -0300

    add

--- app/models/novedades/ProduccioImagenesPuestoLaboralPeriodo.java
+++ app/models/novedades/ProduccioImagenesPuestoLaboralPeriodo.java
@@ -2,6 +2,7 @@ package models.novedades;
 
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,6 +11,9 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import com.avaje.ebean.Ebean;
+import com.avaje.ebean.SqlRow;
 
 import models.Periodo;
 import models.haberes.PuestoLaboral;
@@ -54,6 +58,24 @@ public class ProduccioImagenesPuestoLaboralPeriodo extends Model{
 	public Date create_date;
 
 	public static Model.Finder<Long,ProduccioImagenesPuestoLaboralPeriodo> find = new Finder<Long,ProduccioImagenesPuestoLaboralPeriodo>(Long.class, ProduccioImagenesPuestoLaboralPeriodo.class);
+
+	public static List<SqlRow> getProduccionMes(Long planificacion_id){
+
+		String sql = "SELECT ag.apellido as apellido, puesto_laboral_id, periodo_id, dias_mes, minutos_profesional, minutos_practicas, valor_minuto, monto_sueldo, monto_especialidad, "+
+				"minutos_exedentes, produccion, planificacion_id " +
+				"FROM produccion_imagenes_puesto_laboral_periodos pi " +
+				"INNER JOIN puestos_laborales pl on pl.id = pi.puesto_laboral_id " +
+				"INNER JOIN legajos l on l.id = pl.legajo_id  " +
+				"INNER JOIN agentes ag on ag.id = l.agente_id  " +
+				"WHERE planificacion_id = :planificacion_id "+
+				"ORDER BY apellido ASC ";
+
+		List<com.avaje.ebean.SqlRow> l = Ebean.createSqlQuery(sql)
+				.setParameter("planificacion_id", planificacion_id)
+				.findList();
+
+		return l;
+	}
 
 
 }
