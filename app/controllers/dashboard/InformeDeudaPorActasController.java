2a3893ea65aadc3cd36ca4f1fa7bb6bd95fb3c66
Marcelo Schell <mschell@proveedor.teco.com.ar>
2025-11-12 21:40:00 -0300

    add

--- app/controllers/dashboard/InformeDeudaPorActasController.java
+++ app/controllers/dashboard/InformeDeudaPorActasController.java
@@ -41,69 +41,69 @@ import views.html.dashboard.informeEstadisticoDeudaProveedores.informeDeudaPorAc
 @Security.Authenticated(Secured.class)
 public class InformeDeudaPorActasController extends Controller {
 
-	
+
 	@CheckPermiso(key = "dashboardInformeDeudaPorActas")
 	public static Result index() {
-		
-		/*Pagination<InformeDeudaPorActa> i = InformeDeudaPorActa.page(RequestVar.get("acta_numero"), 
-																	 RequestVar.get("orden_provision"), 
-																	 RequestVar.get("proveedor_id"), 
-																	 RequestVar.get("expediente_id"), 
-																	 RequestVar.get("ejercicio"), 
-																	 RequestVar.get("deuda_mayor"), 
-																	 RequestVar.get("deuda_menor"), 
+
+		/*Pagination<InformeDeudaPorActa> i = InformeDeudaPorActa.page(RequestVar.get("acta_numero"),
+																	 RequestVar.get("orden_provision"),
+																	 RequestVar.get("proveedor_id"),
+																	 RequestVar.get("expediente_id"),
+																	 RequestVar.get("ejercicio"),
+																	 RequestVar.get("deuda_mayor"),
+																	 RequestVar.get("deuda_menor"),
 																	 RequestVar.get("profe"),
-																	 false, 
-																	 RequestVar.get("dolar"), 
-																	 RequestVar.get("deposito_id"), 
+																	 false,
+																	 RequestVar.get("dolar"),
+																	 RequestVar.get("deposito_id"),
 																	 RequestVar.get("tipo_acta"),
 																	 RequestVar.get("tipo_cuenta_id"),
 																	 RequestVar.get("tipo_moneda"));*/
 		InformeDeudaPorActaMaterializada.actualizarVistaMaterializada();
-		Pagination<InformeDeudaPorActaMaterializada> i = InformeDeudaPorActaMaterializada.page(RequestVar.get("acta_numero"), 
-																								 RequestVar.get("orden_provision"), 
-																								 RequestVar.get("proveedor_id"), 
-																								 RequestVar.get("expediente_id"), 
-																								 RequestVar.get("ejercicio"), 
-																								 RequestVar.get("deuda_mayor"), 
-																								 RequestVar.get("deuda_menor"), 
+		Pagination<InformeDeudaPorActaMaterializada> i = InformeDeudaPorActaMaterializada.page(RequestVar.get("acta_numero"),
+																								 RequestVar.get("orden_provision"),
+																								 RequestVar.get("proveedor_id"),
+																								 RequestVar.get("expediente_id"),
+																								 RequestVar.get("ejercicio"),
+																								 RequestVar.get("deuda_mayor"),
+																								 RequestVar.get("deuda_menor"),
 																								 RequestVar.get("profe"),
-																								 false, 
-																								 RequestVar.get("dolar"), 
-																								 RequestVar.get("deposito_id"), 
+																								 false,
+																								 RequestVar.get("dolar"),
+																								 RequestVar.get("deposito_id"),
 																								 RequestVar.get("tipo_acta"),
 																								 RequestVar.get("tipo_cuenta_id"),
 																								 RequestVar.get("tipo_moneda"),
-																								 RequestVar.get("emergencia"));
-		
+																								 RequestVar.get("emergencia"),"");
+
 		return ok(informeDeudaPorActas.render(i, form().bindFromRequest()));
 	}
-	
+
 	@CheckPermiso(key = "dashboardInformeDeudaPorActas")
 	public static Result archivoDeudaPorActas () {
 
-		
+
 		InformeDeudaPorActaMaterializada.actualizarVistaMaterializada();
-		Pagination<InformeDeudaPorActaMaterializada> informe = InformeDeudaPorActaMaterializada.page(RequestVar.get("acta_numero"), 
-																			RequestVar.get("orden_provision"), 
-																			RequestVar.get("proveedor_id"), 
-																			RequestVar.get("expediente_id"), 
-																			RequestVar.get("ejercicio"), 
-																			RequestVar.get("deuda_mayor"), 
-																			RequestVar.get("deuda_menor"), 
+		Pagination<InformeDeudaPorActaMaterializada> informe = InformeDeudaPorActaMaterializada.page(RequestVar.get("acta_numero"),
+																			RequestVar.get("orden_provision"),
+																			RequestVar.get("proveedor_id"),
+																			RequestVar.get("expediente_id"),
+																			RequestVar.get("ejercicio"),
+																			RequestVar.get("deuda_mayor"),
+																			RequestVar.get("deuda_menor"),
 																			RequestVar.get("profe"),
-																			false, 
-																			RequestVar.get("dolar"), 
+																			false,
+																			RequestVar.get("dolar"),
 																			RequestVar.get("deposito_id"),
 																			RequestVar.get("tipo_acta"),
 																			RequestVar.get("tipo_cuenta_id"),
 																			RequestVar.get("tipo_moneda"),
-																			RequestVar.get("emergencia"));
-		
+																			RequestVar.get("emergencia"),"");
+
 		String dirTemp = System.getProperty("java.io.tmpdir");
-		
+
 		Integer fila = 10;
-		
+
 		Integer celdaActa 			 = 0;
 		Integer celdaEjercicio 		 = 1;
 		Integer celdaFecha 			 = 2;
@@ -118,12 +118,12 @@ public class InformeDeudaPorActasController extends Controller {
 		Integer celdaTotalPagado 	 = 11;
 		Integer celdaTotalDeuda 	 = 12;
 		Integer celdaDetalleExpediente = 13;
-		
+
 		try {
 			File archivo = new File(dirTemp+"/informe-deuda-actas-"+Usuario.getUsuarioSesion()+".xls");
 			if(archivo.exists()) archivo.delete();
 			FileInputStream file = new FileInputStream(Play.application().getFile("conf/resources/reportes/dashboard/informe-deuda-actas.xls"));
-			
+
 			Workbook libro = new HSSFWorkbook(file);
 			FileOutputStream archivoTmp = new FileOutputStream(archivo);
 			Sheet hoja = libro.getSheetAt(0);
@@ -136,23 +136,23 @@ public class InformeDeudaPorActasController extends Controller {
 			style.setDataFormat(libro.createDataFormat().getFormat("$ #,##0.00"));
 
 		    for (InformeDeudaPorActaMaterializada i : informe.getList()) {
-		    	
+
 				Row f = hoja.createRow(fila);
-				
+
 				//Acta número
 				celda = f.createCell(celdaActa);
 				celda.setCellValue( i.acta_numero );
-				
+
 				Logger.debug("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx "+i.acta_numero);
-				
+
 				//ejercicio
 				celda = f.createCell(celdaEjercicio);
 				celda.setCellValue((i.ejercicio != null)?i.ejercicio.nombre:"");
-				
+
 				//fecha
 				celda = f.createCell(celdaFecha);
 				celda.setCellValue(utils.DateUtils.formatDate(i.fecha));
-				
+
 				//Número Op
 				celda = f.createCell(celdaOp);
 				String orden = "";
@@ -164,73 +164,73 @@ public class InformeDeudaPorActasController extends Controller {
 				//Celda Exp
 				celda = f.createCell(celdaExp);
 				celda.setCellValue(i.expediente.getExpedienteEjercicio());
-				
+
 				//Celda cuenta
 				celda = f.createCell(celdaCuenta);
 				celda.setCellValue((i.tipoCuenta != null)?i.tipoCuenta.nombre:"");
-				
+
 				//Celda Institucion
 				celda = f.createCell(celdaInst);
 				celda.setCellValue(i.ordenCompra.deposito.nombre);
-				
+
 				//Celda Rubro
 				celda = f.createCell(celdaRubro);
 				celda.setCellValue(i.ordenCompra.ordenRubro.nombre);
-	
+
 				//Celda Proveedor
 				celda = f.createCell(celdaProveedor);
 				celda.setCellValue(i.proveedor.nombre);
-				
+
 				//Celda total total acta
 				celda = f.createCell(celdaTotalActa);
 				celda.setCellValue(i.totalActa.doubleValue());
 				celda.setCellStyle(style);
-				
+
 				//Celda total total acta
 				celda = f.createCell(celdaTotalAutorizado);
 				celda.setCellValue(i.totalAutorizado.doubleValue());
 				celda.setCellStyle(style);
-				
+
 				//Celda total total pagado
 				celda = f.createCell(celdaTotalPagado);
 				celda.setCellValue(i.totalPagado.doubleValue());
 				celda.setCellStyle(style);
 
-			
-				
-				
+
+
+
 				//Celda total de deudas
 				celda = f.createCell(celdaTotalDeuda);
 				celda.setCellValue(i.totalDeuda.doubleValue());
 				celda.setCellStyle(style);
-				
+
 				//Celda detalle expediente
 				celda = f.createCell(celdaDetalleExpediente);
 				celda.setCellValue(i.expediente.descripcion);
 				celda.setCellStyle(style);
-				
-				
-				
+
+
+
 				fila++;
-			}  
-			
-		    
-		    
-		    
-			libro.write(archivoTmp);   
-			  
+			}
+
+
+
+
+			libro.write(archivoTmp);
+
 			Writer out = new BufferedWriter(new OutputStreamWriter(archivoTmp, "UTF8"));
 			out.flush();
 			out.close();
-			
-			
+
+
 			return ok(archivo);
-			
+
 		} catch (IOException e) {
 		    e.printStackTrace();
 		}
-		
-		
+
+
 		return ok();
 	}
 }
