e692e320a7ffd3879f21f1c2c1d9dbdeab5d846d
Marcelo Schell <mschell@proveedor.teco.com.ar>
2026-01-05 09:40:06 -0300

    ADD

--- app/models/TipoTarea.java
+++ app/models/TipoTarea.java
@@ -11,31 +11,35 @@ import javax.persistence.Table;
 import play.db.ebean.Model;
 import play.db.ebean.Model.Finder;
 
- 
-@Entity 
+
+@Entity
 @Table(name = "tipos_tareas")
 public class TipoTarea extends Model{
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	public static final Long MAIL_DEUDA_RA = (long)1;
 	public static final Long MAIL_DEUDA_DESTACADOS = (long)2;
 	public static final Long MAIL_DEUDA_OTROS = (long)3;
 	public static final Long MAIL_PAGADO_NOENTREGADOS = (long)4;
-		
-	@Id  
+
+	public static final Long MAIL_CONTROL_ASISTENCIA_PERSONAL = (long)5;
+	public static final Long MAIL_CONTROL_ASISTENCIA_DOCENCIA = (long)6;
+	public static final Long MAIL_CONTROL_ASISTENCIA_MEDICINA = (long)7;
+
+	@Id
 	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="tipos_tareas_id_seq")
-	public Long id; 
-	public String nombre; 
-	
+	public Long id;
+	public String nombre;
+
 	public static Model.Finder<Long,TipoTarea> find = new Finder<Long,TipoTarea>(Long.class, TipoTarea.class);
-	
+
 	public List<TipoTarea> getDataSuggest(String input,Integer limit){
 		List<TipoTarea> l = find.where()
 				.ilike("nombre", "%"+input+"%")
 				.setMaxRows(limit).orderBy("nombre")
-			    .findList();  
-		
+			    .findList();
+
 		return l;
 	}
 }
