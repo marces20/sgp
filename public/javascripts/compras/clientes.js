a0392300fbba224d6f8b1294245288a2a219b9fd
Marcelo Schell <mschell@proveedor.teco.com.ar>
2026-03-03 08:04:57 -0300

    add

--- public/javascripts/compras/clientes.js
+++ public/javascripts/compras/clientes.js
@@ -1,8 +1,10 @@
 $( function(){
-	 
+
 	$('#searchObraSocial').modalSearch();
-	
+
 	$("#cuit2").mask("99999999999");
 	$("#dni").mask("99999999");
 	$("#cie").mask("99999999");
+	$("#cie2").mask("99999999");
+
 });
