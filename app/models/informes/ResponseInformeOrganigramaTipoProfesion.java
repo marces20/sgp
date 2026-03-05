73219f8bb881845760e81b4d06fc56ddc31150ba
Marcelo Schell <mschell@proveedor.teco.com.ar>
2026-03-05 10:12:56 -0300

    add

---
+++ app/models/informes/ResponseInformeOrganigramaTipoProfesion.java
@@ -0,0 +1,21 @@
+package models.informes;
+
+import java.math.BigDecimal;
+
+public class ResponseInformeOrganigramaTipoProfesion {
+
+	public String name = "";
+	public Integer medicos = 0;
+	public Integer enfermero = 0;
+	public Integer tecnicos = 0;
+	public Integer tecnicoAsistencial = 0;
+	public Integer auxiliares = 0;
+	public Integer auxiliaresAsistencial = 0;
+	public Integer adminitrativo = 0;
+	public Integer adminitrativoAsistencial = 0;
+	public Integer profesion = 0;
+	public Integer profesionAsistencia = 0;
+	public Integer total = 0;
+	public String color = "";
+}
+
