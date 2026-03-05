fa1a950208dd6b813bbf67c28afffa80fcc7e091
Marcelo Schell <mschell@proveedor.teco.com.ar>
2026-03-05 12:53:38 -0300

    add

---
+++ app/models/informes/ResponseInformeOrganigramaTipoRelacionLaboral.java
@@ -0,0 +1,21 @@
+package models.informes;
+
+public class ResponseInformeOrganigramaTipoRelacionLaboral {
+
+	public String name = "";
+
+	public Integer contratoRelacionParque = 0;
+	public Integer contratoSinRelacionParque = 0;
+
+	public Integer contratoRelacionConvenioMinisterio = 0;
+
+	public Integer plantaPermanenteOtrasEntidades = 0;
+	public Integer contratoRelacionOtrasEntidades = 0;
+	public Integer adscriptoOtrasEntidades = 0;
+	public Integer contratoConvenioNacion = 0;
+	public Integer plantaTemporariaOtrasEntidades = 0;
+	public Integer otro = 0;
+
+	public Integer total = 0;
+	public String color = "";
+}
