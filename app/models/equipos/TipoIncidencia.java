33bcb8f51effc4c8b7de30af11fa44cfde499341
Marcelo Schell <marces2000@gmail.com>
2026-07-31 17:18:21 -0300

    add

--- app/models/equipos/TipoIncidencia.java
+++ app/models/equipos/TipoIncidencia.java
@@ -6,7 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import models.TipoEmbargo;
+
 import play.data.validation.Constraints.Required;
 import play.db.ebean.Model;
 
