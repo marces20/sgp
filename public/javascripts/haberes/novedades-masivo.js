61443ffbea9ca82a041deeef3bef9c7bef921667
Marcelo Schell <marces2000@gmail.com>
2026-08-06 09:50:31 -0300

    add

--- public/javascripts/haberes/novedades-masivo.js
+++ public/javascripts/haberes/novedades-masivo.js
@@ -1,35 +1,47 @@
 $( function() {
 
-	$('#searchPeriodoInicio, #searchPeriodoFin').modalSearch();
+	$('#searchPeriodoInicio, #searchPeriodoFin,#searchAgente').modalSearch();
 
-	
-	
+	if($("#agente").length){
+		var options = {
+				script:"/suggestAgente/",
+				varname:"",
+				json:true,
+				shownoresults:true,
+				maxresults:6,
+				callback: function (obj) {
+											$("#agente_id").val(obj.id);
+										 }
+			};
+		var as_json = new bsn.AutoSuggest('agente', options);
+	}
+
 	var options = {
 			script:"/contabilidad/suggestPeriodo/",
 			varname:"",
 			json:true,
 			shownoresults:true,
 			maxresults:6,
-			callback: function (obj) { 
-										$("#periodo_inicio_id").val(obj.id); 
+			callback: function (obj) {
+										$("#periodo_inicio_id").val(obj.id);
 										$("#periodo_inicio_id").change();
 									 }
 		};
 	var as_json = new bsn.AutoSuggest('periodo_inicio', options);
-	
+
 	var options = {
 			script:"/contabilidad/suggestPeriodo/",
 			varname:"",
 			json:true,
 			shownoresults:true,
 			maxresults:6,
-			callback: function (obj) { 
-										$("#periodo_hasta_id").val(obj.id); 
+			callback: function (obj) {
+										$("#periodo_hasta_id").val(obj.id);
 										$("#periodo_hasta_id").change();
 									 }
 		};
 	var as_json = new bsn.AutoSuggest('periodo_fin', options);
-	
-	
-	
+
+
+
 });
